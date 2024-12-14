ffc3e249eb8478a38dba469d5399f7e7d4dd7374
Mark Balajadia <markb.freelance@gmail.com>
2024-12-15 00:51:16 +0800

    layout change test push

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,4 @@
 import '../styles/globals.css';
-import Header from '@/components/Header';
-import Footer from '@/components/footer';
 
 export const metadata = {
   title: 'APEC main Website',
@@ -14,7 +12,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className="min-h-screen">
 
         {/* Add the Header here */}
-        <Header />
 
         {/* Main content of the page */}
         <div className="max-w-7xl mx-auto p-4">
@@ -22,7 +19,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </div>
 
         {/* Add the Footer here */}
-        <Footer />
 
       </body>
     </html>
