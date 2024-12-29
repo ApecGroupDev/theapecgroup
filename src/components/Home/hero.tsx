d28221dfec792a3d8df1fa2ccc3be9bf13dd9b15
Mark Balajadia <markb.freelance@gmail.com>
2024-12-29 19:57:13 +0800

    moved hero content into left part - 1080p reso

--- src/components/Home/hero.tsx
+++ src/components/Home/hero.tsx
@@ -1,6 +1,6 @@
 const Hero: React.FC = () => {
   return (
-    <div className="relative flex items-center justify-center min-h-screen bg-transparent">
+    <div className="relative flex items-center min-h-screen bg-transparent">
       <div className="text-white pl-40">
         <p className="text-6xl font-bold mb-4 text-black">YOUR TOP CHOICE</p>
         <p className="text-8xl mb-8 italic text-red-600 tracking-wide">ONE-STOP SHOP</p>
