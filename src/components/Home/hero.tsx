da6c5a9cbd4538f5c52be343ac362ee45bcef44b
Mark Balajadia <markb.freelance@gmail.com>
2025-01-08 03:27:26 +0800

    adjusted font size on lg screens

--- src/components/Home/hero.tsx
+++ src/components/Home/hero.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Hero: React.FC = () => {
   return (
-    <div className="relative flex flex-col-reverse md:flex-row items-center justify-between min-h-[58rem] bg-transparent px-8 md:px-16 lg:px-40 pt-20 md:pt-32">
+    <div className="relative flex flex-col-reverse md:flex-row items-center justify-between min-h-screen bg-transparent px-8 md:px-16 lg:px-40">
       {/* Left Text Section */}
       <div className="text-center md:text-left text-black">
-        <p className="text-4xl md:text-5xl lg:text-6xl font-extrabold mb-4">YOUR TOP CHOICE</p>
-        <p className="text-5xl md:text-6xl lg:text-7xl mb-8 italic text-red-500 tracking-wider">ONE-STOP SHOP</p>
-        <p className="text-3xl md:text-4xl lg:text-5xl font-medium mb-4">ATLANTA COMPANY</p>
+        <p className="text-4xl md:text-5xl lg:text-7xl font-extrabold mb-4">YOUR TOP CHOICE</p>
+        <p className="text-5xl md:text-6xl lg:text-8xl mb-8 italic text-red-500 tracking-wider">ONE-STOP SHOP</p>
+        <p className="text-3xl md:text-4xl lg:text-7xl font-medium mb-4">ATLANTA COMPANY</p>
       </div>
 
       {/* Right Image Section */}
