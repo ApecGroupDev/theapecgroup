5d84b7aa4b39e0d824e930af86b3cde484d6b349
Mark Balajadia <markb.freelance@gmail.com>
2025-01-09 08:14:30 +0800

    2k reso new padding-x

--- src/components/Home/hero.tsx
+++ src/components/Home/hero.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Hero: React.FC = () => {
   return (
-    <div className="relative flex flex-col-reverse md:flex-row items-center justify-between min-h-screen bg-transparent px-8 md:px-16 md:pt-16 lg:px-40 xl:px-28 macbook-pro-16:pb-28">
+    <div className="relative flex flex-col-reverse md:flex-row items-center justify-between min-h-screen bg-transparent px-8 md:px-16 md:pt-16 lg:px-40 xl:px-28 macbook-pro-16:pb-28 2k:px-40">
       {/* Left Text Section */}
       <div className="text-center md:text-left 2xl:mb-20 text-black macbook-pro-14:mb-20">
         <p className="text-4xl md:text-5xl lg:text-5xl xl:text-5xl macbook-pro-14:text-6xl 2xl:text-7xl 2k:text-8xl font-extrabold">YOUR TOP CHOICE</p>
