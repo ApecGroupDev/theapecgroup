884ba2a4ae39154950eea1606e04c183cb4002ee
Mark Balajadia <markb.freelance@gmail.com>
2025-01-10 10:17:59 +0800

    feature boxes h correction up to lg screen

--- src/components/Home/hero.tsx
+++ src/components/Home/hero.tsx
@@ -4,7 +4,7 @@ const Hero: React.FC = () => {
   return (
     <div className="relative flex flex-col-reverse md:flex-row items-center justify-between bg-transparent px-8 md:px-16 2xl:pt-16 lg:pt-0 lg:px-16 xl:px-28 2k:px-40">
       {/* Left Text Section */}
-      <div className="text-center md:text-left 2xl:mb-20 text-black md:mt-60 lg:mt-0 xl:mt-0 2k:mt-16">
+      <div className="text-center md:text-left 2xl:mb-20 text-black md:mt-60 lg:mt-12 xl:mt-0 2k:mt-16">
         <p className="text-4xl md:text-4xl lg:text-4xl xl:text-5xl mac-14:text-6xl 2xl:text-7xl 2k:text-8xl font-extrabold">YOUR TOP CHOICE</p>
         <p className="text-5xl md:text-5xl lg:text-5xl xl:text-6xl mac-14:text-7xl 2xl:text-8xl 2k:text-9xl xl:mt-4 2xl:mt-4 italic text-red-500 tracking-wider whitespace-nowrap">ONE-STOP SHOP</p>
         <p className="text-3xl md:text-4xl lg:text-4xl xl:text-5xl mac-14:text-6xl 2xl:text-7xl 2k:text-8xl xl:mt-4 2xl:mt-4 font-medium">ATLANTA COMPANY</p>
@@ -15,7 +15,7 @@ const Hero: React.FC = () => {
         <img
           src="/products/Pump.png"
           alt="Hero"
-          className="rounded-lg object-cover w-full max-w-[500px] md:max-w-[600px] lg:max-w-[400px] xl:max-w-[600px] xl:mt-12 2xl:max-w-[700px] 2k:max-w-[900px]"
+          className="rounded-lg object-cover w-full max-w-[500px] md:max-w-[600px] lg:max-w-[320px] xl:max-w-[600px] xl:mt-12 2xl:max-w-[700px] 2k:max-w-[900px]"
         />
       </div>
     </div>
