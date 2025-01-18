a7599ff604926b395e990da4a7190e7920919371
Mark Balajadia <markb.freelance@gmail.com>
2025-01-18 19:24:20 +0800

    header adjusted to screen sizes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -36,19 +36,19 @@ const Header: React.FC = () => {
     >
       <div className="container mx-auto relative flex items-center md:py-4">
         {/* Logo Section */}
-        <div className="absolute left-0 top-1/2 transform -translate-y-1/2 md:hidden">
+        <div className="absolute md:left-4 md:top-8 xl:left-6 xl:top-10 mac-16:top-14 2xl:-left-14 2xl:top-16 2k:left-8 2k:top-20 transform -translate-y-1/2">
           <Link href="/">
             <img
               src="/logos/APEC.png"
               alt="Logo"
-              className="w-800:h-18 w-900:h-20 lg:h-24 2k:h-28 w-auto"
+              className="md:h-16 xl:h-20 mac-16:h-24 2xl:h-28 2k:h-32"
             />
           </Link>
         </div>
 
         {/* Navigation Links */}
         <nav
-          className={`flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4 mx-auto`}
+          className={`flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4 2xl:space-x-8 mx-auto`}
         >
           {[
             "/",
@@ -62,12 +62,12 @@ const Header: React.FC = () => {
             <Link
               key={path}
               href={path}
-              className={`md:text-sm text-gray-900 hover:text-red-600 transition-colors duration-200 relative ${pathname === path ? "text-red-600" : ""
+              className={`md:text-sm mac-14:text-lg mac-16:text-lg 2xl:text-xl text-gray-900 hover:text-red-600 transition-colors duration-200 relative ${pathname === path ? "text-red-600" : ""
                 }`}
             >
               {path === "/" ? "HOME" : path.replace("/", "").toUpperCase()}
               {pathname === path && (
-                <span className="absolute left-0 right-0 top-5 bottom-0 h-0.5 bg-red-600" />
+                <span className="absolute left-0 right-0 md:top-5 mac-14:top-6 mac-16:top-6 2xl:top-7 bottom-0 h-0.5 bg-red-600" />
               )}
             </Link>
           ))}
