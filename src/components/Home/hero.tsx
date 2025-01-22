c9b6c30490a1bf406e6d00c3282ff4a794c3d173
Mark Balajadia <markb.freelance@gmail.com>
2025-01-23 01:10:55 +0800

    removed white outlines on hero (mobile)

--- src/components/Home/hero.tsx
+++ src/components/Home/hero.tsx
@@ -5,13 +5,13 @@ const Hero: React.FC = () => {
     <div className="flex flex-col md:flex-row items-center justify-between bg-transparent md:px-12">
       {/* Left Text Section */}
       <div className="-space-y-1.5 md:pb-0 w-800:pb-10 w-900:pb-8 lg:pb-5">
-        <p className="text-3xl md:text-xl w-900:text-2xl lg:text-3xl mac-14:text-4xl mac-16:text-5xl 2xl:text-5xl 2k:text-6xl font-bold whitespace-nowrap outlined-text-white">
+        <p className="text-3xl md:text-xl w-900:text-2xl lg:text-3xl mac-14:text-4xl mac-16:text-5xl 2xl:text-5xl 2k:text-6xl font-bold whitespace-nowrap">
           YOUR TOP CHOICE
         </p>
-        <p className="text-4xl md:text-3xl w-900:text-4xl lg:text-5xl mac-14:text-6xl mac-16:text-7xl 2xl:text-7xl 2k:text-8xl font-regular italic text-red-500 tracking-wider whitespace-nowrap outlined-text-white">
+        <p className="text-4xl md:text-3xl w-900:text-4xl lg:text-5xl mac-14:text-6xl mac-16:text-7xl 2xl:text-7xl 2k:text-8xl font-regular italic text-red-500 tracking-wider whitespace-nowrap">
           ONE-STOP SHOP
         </p>
-        <p className="text-3xl md:text-xl w-900:text-2xl lg:text-3xl mac-14:text-4xl mac-16:text-5xl 2xl:text-5xl 2k:text-6xl font-regular outlined-text-white">
+        <p className="text-3xl md:text-xl w-900:text-2xl lg:text-3xl mac-14:text-4xl mac-16:text-5xl 2xl:text-5xl 2k:text-6xl font-regular">
           ATLANTA COMPANY
         </p>
       </div>
