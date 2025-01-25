36c52ae0ed4ead1543b8fe99e6712007d00080fe
Mark Balajadia <markb.freelance@gmail.com>
2025-01-25 11:54:10 +0800

    services layout and page tsx files

---
+++ src/app/services/layout.tsx
@@ -0,0 +1,22 @@
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+
+export default function ServicesLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div
+      style={{
+        backgroundImage: "",
+        backgroundSize: "cover",
+        backgroundPosition: "center top",
+        backgroundRepeat: "no-repeat",
+        minHeight: "100vh",
+      }}>
+
+      <Header />
+
+      <div className="w-full">{children}</div>
+
+      <Footer />
+    </div>
+  );
+}
---
+++ src/app/services/page.tsx
@@ -0,0 +1,9 @@
+const Services: React.FC = () => {
+  return (
+    <div className="min-h-96 flex items-center justify-center">
+      <p className="text-9xl">TEMPORARY CONTENT</p>
+    </div>
+  )
+}
+
+export default Services;
