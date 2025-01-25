9ffe87243484304919377615f1097ab769129cfb
Mark Balajadia <markb.freelance@gmail.com>
2025-01-25 12:21:29 +0800

    careers layout and page tsx files

---
+++ src/app/careers/layout.tsx
@@ -0,0 +1,23 @@
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+
+export default function CareersLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div
+      style={{
+        backgroundImage: "",
+        backgroundSize: "cover",
+        backgroundPosition: "center top",
+        backgroundRepeat: "no-repeat",
+        minHeight: "100vh",
+      }}>
+
+      <Header />
+
+      <div className="w-full">{children}</div>
+
+      <Footer />
+
+    </div>
+  );
+}
