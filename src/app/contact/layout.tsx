1a8e0100b1570b33ff2053ff9d2b5af5d800dbb1
Mark Balajadia <markb.freelance@gmail.com>
2025-01-26 03:35:40 +0800

    contacts layout

---
+++ src/app/contact/layout.tsx
@@ -0,0 +1,24 @@
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+
+export default function ContactLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div
+      style={{
+        backgroundImage: "",
+        backgroundSize: "cover",
+        backgroundPosition: "center top",
+        backgroundRepeat: "no-repeat",
+        minHeight: "100vh",
+      }}
+    >
+
+      <Header />
+
+      <div className="w-full">{children}</div>
+
+      <Footer />
+
+    </div>
+  )
+}
