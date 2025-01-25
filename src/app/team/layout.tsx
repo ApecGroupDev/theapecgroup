dd5bf0144e345a6ab5679aa26ccaf8aaa6e3a189
Mark Balajadia <markb.freelance@gmail.com>
2025-01-25 11:22:37 +0800

    layout tsx for team page

---
+++ src/app/team/layout.tsx
@@ -0,0 +1,25 @@
+import Header from "@/components/header"
+import Footer from "@/components/footer"
+
+export default function TeamLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div
+      style={{
+        backgroundImage: "",
+        backgroundSize: "cover",
+        backgroundPosition: "center top",
+        backgroundRepeat: "no-repeat",
+        minHeight: "100vh",
+
+      }}
+    >
+
+      <Header />
+
+      <div className="w-full">{children}</div>
+
+      <Footer />
+
+    </div>
+  )
+}
