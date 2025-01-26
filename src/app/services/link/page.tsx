2bf967eddb59b173db3d6461a6a552f148daed99
Mark Balajadia <markb.freelance@gmail.com>
2025-01-26 13:30:33 +0800

    APEC link

---
+++ src/app/services/link/page.tsx
@@ -0,0 +1,9 @@
+const Link: React.FC = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <p className="text-9xl text-wrap">LINK CONTENT</p>
+    </div>
+  )
+}
+
+export default Link;
