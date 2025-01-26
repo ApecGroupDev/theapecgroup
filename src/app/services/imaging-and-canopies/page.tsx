334e7139ccd85761c27eca052a05a7f962b2a5fa
Mark Balajadia <markb.freelance@gmail.com>
2025-01-26 13:11:07 +0800

    APEC imaging and canopies

---
+++ src/app/services/imaging-and-canopies/page.tsx
@@ -0,0 +1,9 @@
+const ImagingAndCanopies: React.FC = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <p className="text-9xl text-wrap">TEMPORARY CONTENT</p>
+    </div>
+  )
+}
+
+export default ImagingAndCanopies;
