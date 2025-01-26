794db42ef2c8ecd900e51edc8c95175b893f464d
Mark Balajadia <markb.freelance@gmail.com>
2025-01-26 12:17:31 +0800

    APEC construction

---
+++ src/app/services/construction/page.tsx
@@ -0,0 +1,9 @@
+const Construction: React.FC = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <p className="text-9xl text-wrap">TEMPORARY CONTENT</p>
+    </div>
+  )
+}
+
+export default Construction;
