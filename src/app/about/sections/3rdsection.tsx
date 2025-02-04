a1c32e4fb7352680602c131e47893b7493ac6899
Mark Balajadia <markb.freelance@gmail.com>
2025-02-05 06:39:38 +0800

    3rd section font size adjustments

--- src/app/about/sections/3rdsection.tsx
+++ src/app/about/sections/3rdsection.tsx
@@ -1,36 +1,36 @@
 const ThirdSection: React.FC = () => {
   return (
-    <div className="h-auto 2xl:min-h-screen flex flex-col items-center justify-center text-white">
+    <div className="flex flex-col items-center justify-center text-white">
       <div className="w-auto flex flex-col">
         {/* Top Section: APEC + OUT and WORK */}
-        <div className="flex gap-16">
+        <div className="flex">
           {/* Left Side (APEC + OUT) */}
           <div className="w-1/5 flex flex-col flex-1">
             {/* Ensuring height matches WORK */}
             <div className="2xl:mt-24">
               {/* APEC + will always (Same Line, Bottom Alignment) */}
-              <div className="outline-dotted rounded-lg flex items-center justify-end">
-                <span className="text-5xl font-semibold">APEC</span>
-                <span className="text-2xl font-extralight tracking-widest ml-2">will always</span>
+              <div className="flex items-center justify-end">
+                <span className="2xl:text-8xl font-semibold">APEC</span>
+                <span className="2xl:text-5xl font-extralight tracking-widest">will always</span>
               </div>
 
               {/* OUT (Aligned at the Top) */}
-              <div className="outline-dotted rounded-lg flex justify-end items-start">
-                <span className="text-10xl font-semibold italic 2xl:-mt-12 2xl:me-6">OUT</span>
+              <div className="flex justify-end items-start">
+                <span className="2xl:text-12xl font-semibold italic 2xl:-mt-16 2xl:me-6">OUT</span>
               </div>
             </div>
           </div>
 
           {/* Right Side (WORK + the competition) */}
-          <div className="w-3/5 flex flex-col items-center">
+          <div className="w-auto flex flex-col items-center">
             {/* WORK */}
-            <div className="w-full outline-dashed rounded-lg flex items-center justify-center">
-              <span className="text-13xl font-bold text-black">WORK</span>
+            <div className="w-full flex items-center justify-center">
+              <span className="2xl:text-14xl 2xl:scale-y-110 font-bold text-black 2xl:mt-6">WORK</span>
             </div>
 
             {/* the competition (Directly Below WORK) */}
-            <div className="w-full outline-dotted rounded-lg flex items-center justify-end 2xl:-mt-24 2xl:ms-32">
-              <span className="text-2xl">the competition</span>
+            <div className="w-full flex items-center justify-end 2xl:-mt-24 2xl:ms-32">
+              <span className="text-5xl tracking-widest">the competition</span>
             </div>
           </div>
         </div>
