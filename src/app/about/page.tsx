f89e9fac9f6d28030455ec34ef2c8d37c0dc58a5
Mark Balajadia <markb.freelance@gmail.com>
2025-02-06 17:10:20 +0800

    about us page footer

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,6 +2,7 @@ import ThirdSection from "./sections/3rdsection";
 import ChooseUs from "./sections/chooseus";
 import Hero from "./sections/hero";
 import HorizontalScrollSection from "./sections/principles";
+import Footer from "./sections/footerAbout";
 
 const About: React.FC = () => {
   return (
@@ -22,6 +23,11 @@ const About: React.FC = () => {
       <div className="2xl:mt-128">
         <ChooseUs />
       </div>
+
+      <div className="2xl:mt-8">
+        <Footer />
+      </div>
+
     </div>
   );
 };
