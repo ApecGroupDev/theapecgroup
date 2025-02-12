b5657041c10c1bfeeb4d4f6c9a2cb88b13314c98
Mark Balajadia <markb.freelance@gmail.com>
2025-02-12 08:09:04 +0800

    scrn-1700 choose us

--- src/app/about/sections/chooseus.tsx
+++ src/app/about/sections/chooseus.tsx
@@ -10,9 +10,9 @@ const ChooseUs: React.FC = () => {
   });
 
   return (
-    <section className="py-20 px-10 flex flex-col items-center text-center scrn-1900:h-240 scrn-2k:h-352 overflow-hidden">
+    <section className="py-20 px-10 flex flex-col items-center text-center scrn-1700:h-256 scrn-1900:h-240 scrn-2k:h-352 overflow-hidden">
       {/* Heading */}
-      <span className="lg:text-6xl xl:text-8xl mac-14:text-8xl scrn-1900:text-8xl scrn-2k:text-9xl font-bold">
+      <span className="lg:text-6xl xl:text-8xl mac-14:text-8xl scrn-1700:mt-14 scrn-1900:text-8xl scrn-2k:text-9xl font-bold">
         Why
         <motion.p
           ref={ref} // Set the ref to the element
@@ -26,13 +26,13 @@ const ChooseUs: React.FC = () => {
       </span>
 
       {/* Content */}
-      <p className="lg:text-xl lg:max-w-2xl xl:max-w-4xl mac-14:max-w-6xl mac-16:max-w-6xl scrn-1900:max-w-6xl xl:text-2xl mac-14:text-2xl scrn-1900:text-3xl scrn-2k:text-4xl leading-normal font-normal scrn-2k:leading-relaxed tracking-wider lg:mt-10 xl:mt-12 mac-14:mt-12 scrn-1900:mt-12">
+      <p className="lg:text-xl lg:max-w-2xl xl:max-w-4xl mac-14:max-w-6xl scrn-1700:max-w-6xl scrn-1900:max-w-6xl xl:text-2xl mac-14:text-2xl scrn-1900:text-3xl scrn-2k:text-4xl leading-normal font-normal scrn-2k:leading-relaxed tracking-wider lg:mt-10 xl:mt-12 mac-14:mt-12 scrn-1900:mt-12">
         Today, APEC Group offers a diverse range of services, including canopy imaging and installation,
         environmental compliance inspections, C-store renovations, EV charging solutions, remote site monitoring,
         and digital marketing for dispensaries. We also provide tailored financing options to help you grow your business.
       </p>
 
-      <p className="lg:text-xl lg:max-w-xl xl:max-w-3xl mac-14:max-w-4xl mac-16:max-w-4xl scrn-1900:max-w-4xl xl:text-2xl mac-14:text-2xl scrn-1900:text-3xl scrn-2k:text-4xl leading-normal font-normal scrn-2k:leading-relaxed tracking-wider lg:mt-8 xl:mt-6 mac-14:mt-8 scrn-1900:mt-8">
+      <p className="lg:text-xl lg:max-w-xl xl:max-w-3xl mac-14:max-w-4xl scrn-1700:max-w-4xl scrn-1900:max-w-4xl xl:text-2xl mac-14:text-2xl scrn-1900:text-3xl scrn-2k:text-4xl leading-normal font-normal scrn-2k:leading-relaxed tracking-wider lg:mt-8 xl:mt-6 mac-14:mt-8 scrn-1900:mt-8">
         With offices in Atlanta, Georgia, and Houston, Texas, we are dedicated to serving businesses nationwide
         with unparalleled expertise and commitment.
       </p>
@@ -43,7 +43,7 @@ const ChooseUs: React.FC = () => {
       {/* Contact Button */}
       <a
         href="mailto:Sales@TheAPECgroup.com"
-        className="px-16 mt-12 md:px-12 md:text-sm lg:px-20 scrn-2k:px-28 py-1 border-2 border-red-600 text-gray-800 lg:text-lg mac-16:text-2xl scrn-1900:text-2xl scrn-2k:text-3xl font-regular rounded-lg hover:bg-red-600 hover:text-white transition duration-200"
+        className="px-16 mt-12 md:px-12 md:text-sm lg:px-20 scrn-2k:px-28 py-1 border-2 border-red-600 text-gray-800 lg:text-lg scrn-1700:text-2xl scrn-1900:text-2xl scrn-2k:text-3xl font-regular rounded-lg hover:bg-red-600 hover:text-white transition duration-200"
       >
         Sales@TheAPECgroup.com
       </a>
