1580412c35710161d0d64a4c6d4bcc8b3a4238df
Mark Balajadia <markb.freelance@gmail.com>
2025-02-16 10:00:18 +0800

    md principles

--- src/app/about/sections/principles.tsx
+++ src/app/about/sections/principles.tsx
@@ -105,12 +105,12 @@ const HorizontalScrollSection: React.FC = () => {
   const sections = [
     {
       content: (
-        <div className="bg-[url('/backgrounds/Mission_bg.png')] bg-no-repeat md:bg-left lg:bg-left bg-center bg-contain w-full scrn-800:h-124 scrn-900:h-136 lg:h-152 xl:h-192 scrn-1500:h-208 scrn-1700:h-256 scrn-1900:h-240 scrn-2k:h-352 overflow-hidden">
-          <div className="flex flex-col ml-auto items-end scrn-800:mt-8 scrn-800:me-8 scrn-900:mt-8 scrn-900:me-10 lg:mt-12 lg:me-12 xl:mt-24 xl:me-20 scrn-1500:mt-20 scrn-1500:me-24 scrn-1900:mt-28 scrn-1900:me-32">
-            <span className="scrn-800:text-5xl scrn-900:text-5xl lg:text-5xl xl:text-7xl scrn-2k:text-9xl italic">Our</span>
+        <div className="bg-[url('/backgrounds/Mission_bg.png')] bg-no-repeat md:bg-left lg:bg-left bg-center bg-contain w-full md:h-116 scrn-800:h-124 scrn-900:h-136 lg:h-152 xl:h-192 scrn-1500:h-208 scrn-1700:h-256 scrn-1900:h-240 scrn-2k:h-352 overflow-hidden">
+          <div className="flex flex-col ml-auto items-end md:mt-6 md:me-6 scrn-800:mt-8 scrn-800:me-8 scrn-900:mt-8 scrn-900:me-10 lg:mt-12 lg:me-12 xl:mt-24 xl:me-20 scrn-1500:mt-20 scrn-1500:me-24 scrn-1900:mt-28 scrn-1900:me-32">
+            <span className="md:text-4xl scrn-800:text-5xl scrn-900:text-5xl lg:text-5xl xl:text-7xl scrn-2k:text-9xl italic">Our</span>
             <motion.span
               ref={missionRef}
-              className="scrn-800:text-6xl scrn-900:text-6xl lg:text-6xl xl:text-8xl scrn-1500:text-8xl scrn-1900:text-8xl scrn-2k:text-10xl font-bold text-red-600"
+              className="md:text-5xl scrn-800:text-6xl scrn-900:text-6xl lg:text-6xl xl:text-8xl scrn-1500:text-8xl scrn-1900:text-8xl scrn-2k:text-10xl font-bold text-red-600"
               initial={{ y: 70, opacity: 0 }}
               animate={{ y: missionInView ? 0 : 50, opacity: missionInView ? 1 : 0 }}
               transition={{ duration: 0.5, ease: 'easeOut' }}
@@ -128,11 +128,11 @@ const HorizontalScrollSection: React.FC = () => {
     },
     {
       content: (
-        <div className="flex flex-col mx-auto items-center lg:mt-10 xl:mt-12 scrn-1500:mt-16 scrn-1900:mt-20 text-center w-full scrn-900:h-136 lg:h-152 xl:h-192 scrn-1500:h-208 scrn-1700:h-256 scrn-1900:h-240 scrn-2k:h-352 overflow-hidden">
-          <span className="scrn-800:text-5xl scrn-900:text-5xl lg:text-5xl xl:text-7xl scrn-2k:text-9xl italic">Our</span>
+        <div className="flex flex-col mx-auto items-center lg:mt-10 xl:mt-12 scrn-1500:mt-16 scrn-1900:mt-20 text-center w-full md:h-116 scrn-800:h-124 scrn-900:h-136 lg:h-152 xl:h-192 scrn-1500:h-208 scrn-1700:h-256 scrn-1900:h-240 scrn-2k:h-352 overflow-hidden">
+          <span className="md:text-4xl scrn-800:text-5xl scrn-900:text-5xl lg:text-5xl xl:text-7xl scrn-2k:text-9xl italic">Our</span>
           <motion.span
             ref={valuesRef}
-            className="scrn-800:text-6xl scrn-900:text-6xl lg:text-6xl xl:text-7xl scrn-1500:text-8xl scrn-1900:text-8xl scrn-2k:text-10xl font-bold text-red-600"
+            className="md:text-5xl scrn-800:text-6xl scrn-900:text-6xl lg:text-6xl xl:text-7xl scrn-1500:text-8xl scrn-1900:text-8xl scrn-2k:text-10xl font-bold text-red-600"
             initial={{ y: 70, opacity: 0 }}
             animate={{ y: valuesInView ? 0 : 50, opacity: valuesInView ? 1 : 0 }}
             transition={{ duration: 0.5, ease: "easeOut" }}
@@ -140,7 +140,7 @@ const HorizontalScrollSection: React.FC = () => {
             VALUES
           </motion.span>
 
-          <div className="scrn-800:mt-12 scrn-900:mt-12 lg:mt-12 xl:mt-8 scrn-1500:mt-12 scrn-1900:mt-20 tracking-widest flex flex-col scrn-800:gap-4 scrn-900:gap-5 lg:gap-5 xl:gap-8 max-w-4xl">
+          <div className="md:mt-8 scrn-800:mt-12 scrn-900:mt-12 lg:mt-12 xl:mt-8 scrn-1500:mt-12 scrn-1900:mt-20 tracking-widest flex flex-col md:gap-3 scrn-800:gap-4 scrn-900:gap-5 lg:gap-5 xl:gap-8 max-w-4xl">
             {/* Customer Commitment */}
             <div className="flex items-center gap-4">
               <CheckCircle size={48} className="text-red-600 sm:hidden xl:block" />
@@ -186,12 +186,12 @@ const HorizontalScrollSection: React.FC = () => {
     },
     {
       content: (
-        <div className="bg-[url('/backgrounds/Vision_bg.png')] bg-no-repeat bg-right bg-contain w-full scrn-900:h-136 lg:h-152 xl:h-192 scrn-1500:h-208 scrn-1700:h-256 scrn-1900:h-240 scrn-2k:h-352 overflow-hidden">
-          <div className="flex flex-col items-start scrn-800:mt-8 scrn-800:ms-8 scrn-900:mt-8 scrn-900:ml-10 lg:mt-12 lg:ms-12 xl:mt-24 xl:ml-20 scrn-1500:ml-24 scrn-1500:mt-20 scrn-1900:ml-32 scrn-1900:mt-28">
-            <span className="scrn-800:text-5xl scrn-900:text-5xl lg:text-5xl xl:text-7xl scrn-1500:text-7xl scrn-1900:text-7xl scrn-2k:text-9xl italic">Our</span>
+        <div className="bg-[url('/backgrounds/Vision_bg.png')] bg-no-repeat bg-right bg-contain w-full md:h-116 scrn-800:h-124 scrn-900:h-136 lg:h-152 xl:h-192 scrn-1500:h-208 scrn-1700:h-256 scrn-1900:h-240 scrn-2k:h-352 overflow-hidden">
+          <div className="flex flex-col items-start md:mt-6 md:ms-6 scrn-800:mt-8 scrn-800:ms-8 scrn-900:mt-8 scrn-900:ml-10 lg:mt-12 lg:ms-12 xl:mt-24 xl:ml-20 scrn-1500:ml-24 scrn-1500:mt-20 scrn-1900:ml-32 scrn-1900:mt-28">
+            <span className="md:text-4xl scrn-800:text-5xl scrn-900:text-5xl lg:text-5xl xl:text-7xl scrn-1500:text-7xl scrn-1900:text-7xl scrn-2k:text-9xl italic">Our</span>
             <motion.span
               ref={visionRef}
-              className="scrn-800:text-6xl scrn-900:text-6xl lg:text-6xl xl:text-8xl scrn-1500:text-8xl scrn-1900:text-8xl scrn-2k:text-10xl font-bold text-red-600"
+              className="md:text-5xl scrn-800:text-6xl scrn-900:text-6xl lg:text-6xl xl:text-8xl scrn-1500:text-8xl scrn-1900:text-8xl scrn-2k:text-10xl font-bold text-red-600"
               initial={{ y: 70, opacity: 0 }}
               animate={{ y: visionInView ? 0 : 50, opacity: visionInView ? 1 : 0 }}
               transition={{ duration: 0.5, ease: "easeOut" }}
@@ -210,7 +210,7 @@ const HorizontalScrollSection: React.FC = () => {
   ];
 
   return (
-    <div className="relative w-full outline-dashed scrn-800:h-124 scrn-900:h-136 lg:h-152 xl:h-192 scrn-1500:h-208 scrn-1700:h-256 scrn-1900:h-240 scrn-2k:h-352 overflow-hidden">
+    <div className="relative w-full outline-dashed md:h-116 scrn-800:h-124 scrn-900:h-136 lg:h-152 xl:h-192 scrn-1500:h-208 scrn-1700:h-256 scrn-1900:h-240 scrn-2k:h-352 overflow-hidden">
       <div
         ref={sectionRef}
         className="w-full overflow-x-auto overflow-y-hidden flex whitespace-nowrap scroll-smooth"
