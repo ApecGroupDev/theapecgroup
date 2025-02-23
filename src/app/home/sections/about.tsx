d98b6dba6104317d4769d6bc8c0e7072daf45f18
Mark Balajadia <markb.freelance@gmail.com>
2025-02-24 07:13:56 +0800

    scrn-1500 about config

--- src/app/home/sections/about.tsx
+++ src/app/home/sections/about.tsx
@@ -11,7 +11,7 @@ const About: React.FC = () => {
   });
 
   return (
-    <div className="w-full bg-transparent flex flex-col md:flex-row px-12 outline-dashed scrn-1700:h-240 scrn-1900:h-256 scrn-2k:h-352">
+    <div className="w-full bg-transparent flex flex-col md:flex-row px-12 outline-dashed scrn-1500:-mt-8 scrn-1500:h-224 scrn-1700:h-240 scrn-1900:h-256 scrn-2k:h-352">
       {/* Left Image Section */}
       <div className="w-full hidden md:block md:w-2/5 xl:w-1/2">
         <Image
@@ -19,34 +19,34 @@ const About: React.FC = () => {
           alt="Hero"
           width={3264}
           height={4601}
-          className="rounded-lg object-cover w-7/12 md:w-full lg:w-11/12 xl:w-9/12 mac-14:w-10/12 mx-auto"
+          className="rounded-lg object-cover w-7/12 md:w-full lg:w-11/12 xl:w-9/12 scrn-1500:w-10/12 mx-auto"
         />
       </div>
 
       {/* Right Text Section */}
-      <div className="text-center md:text-left md:w-3/5 lg:w-1/2 xl:mt-16 flex flex-col lg:mx-auto mac-14:px-10 mac-14:mt-20 scrn-1700:pe-20 scrn-1700:mt-20 scrn-1900:pe-20 scrn-1900:mt-24 scrn-2k:pe-32 scrn-2k:mt-36">
+      <div className="text-center md:text-left md:w-3/5 lg:w-1/2 xl:mt-16 flex flex-col lg:mx-auto scrn-1500:px-10 scrn-1500:mt-20 scrn-1700:pe-20 scrn-1700:mt-20 scrn-1900:pe-20 scrn-1900:mt-24 scrn-2k:pe-32 scrn-2k:mt-36">
         {/* Animated Welcome Text */}
         <motion.p
           ref={ref} // Set the ref to the element
-          className="text-4xl md:text-4xl xl:text-5xl mac-14:text-6xl scrn-1700:text-6xl scrn-1900:text-7xl scrn-2k:text-8xl font-semibold italic"
+          className="text-4xl md:text-4xl xl:text-5xl scrn-1500:text-6xl scrn-1700:text-6xl scrn-1900:text-7xl scrn-2k:text-8xl font-semibold italic"
           initial={{ y: 70, opacity: 0 }}
           animate={{ y: inView ? 0 : 50, opacity: inView ? 1 : 0 }}
           transition={{ duration: 0.5, ease: "easeOut" }}
         >
           Welcome to
         </motion.p>
-        <p className="text-5xl md:text-5xl xl:text-6xl mac-14:text-7xl scrn-1700:text-7xl scrn-1900:text-8xl scrn-2k:text-9xl font-extrabold text-red-600">
+        <p className="text-5xl md:text-5xl xl:text-6xl scrn-1500:text-7xl scrn-1700:text-7xl scrn-1900:text-8xl scrn-2k:text-9xl font-extrabold text-red-600">
           APEC
         </p>
-        <p className="text-2xl md:text-xl lg:text-2xl mac-14:text-3xl scrn-1700:text-3xl scrn-1900:text-4xl scrn-2k:text-5xl font-medium text-gray-800">
+        <p className="text-2xl md:text-xl lg:text-2xl scrn-1500:text-3xl scrn-1700:text-3xl scrn-1900:text-4xl scrn-2k:text-5xl font-medium text-gray-800">
           Petroleum Equipment Services
         </p>
-        <p className="mt-4 scrn-1900:mt-12 text-justify md:text-sm w-900:text-base lg:text-base xl:text-lg mac-14:text-xl scrn-1700:text-2xl scrn-1900:text-2xl scrn-1900:tracking-widest scrn-2k:text-3xl text-gray-800">
+        <p className="mt-4 scrn-1900:mt-12 text-justify md:text-sm w-900:text-base lg:text-base xl:text-lg scrn-1500:text-xl scrn-1700:text-2xl scrn-1900:text-2xl scrn-1900:tracking-widest scrn-2k:text-3xl text-gray-800">
           The APEC group you know today started out originally as Atlanta Petroleum Equipment Company in September of 1989.
           From our modest beginning as a small service company, we have grown to a leader in our industry offering a full
           line of petroleum equipment, installation, repair and support services.
         </p>
-        <p className="mt-4 md:text-sm text-justify w-900:text-base lg:text-base xl:text-lg mac-14:text-xl scrn-1700:text-2xl scrn-1900:text-2xl scrn-1900:tracking-widest scrn-2k:text-3xl text-gray-800">
+        <p className="mt-4 md:text-sm text-justify w-900:text-base lg:text-base xl:text-lg scrn-1500:text-xl scrn-1700:text-2xl scrn-1900:text-2xl scrn-1900:tracking-widest scrn-2k:text-3xl text-gray-800">
           APEC is committed to providing only the highest quality products and services available. Our industry continues
           to evolve technically year after year. APEC is committed to staying on the leading edge of all the latest innovations
           and technologies to not only support our customer’s needs but so we can also be the industry experts you can rely on.
