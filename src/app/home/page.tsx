f8e9e7fd71c8862d53509b4ff948592c90bdb4d3
Mark Balajadia <markb.freelance@gmail.com>
2025-02-23 18:39:57 +0800

    removed all margins and className on page.tsx / Homepage

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -9,19 +9,19 @@ const Home: React.FC = () => {
   return (
     <div className='flex flex-col'>
       {/* Sections */}
-      <div className='mt-40 md:mt-12 w-800:mt-14'>
+      <div>
         <Hero />
       </div>
 
-      <div className='mt-40 md:mt-12'>
+      <div>
         <About />
       </div>
 
-      <div className='mt-40 md:mt-12 xl:mt-12 mac-16:mt-20 2xl:mt-24 2k:mt-32'>
+      <div>
         <FeatureBoxes />
       </div>
 
-      <div className='mt-40 md:mt-40 md:mb-28 lg:mt-60 lg:mb-40 xl:mt-72 xl:mb-40 mac-14:mt-72 mac-16:mt-96 2xl:mt-96 2xl:mb-56 2k:mt-96 scrn-2k:m-0' >
+      <div>
         <MembershipLogosCarousel />
       </div>
 
