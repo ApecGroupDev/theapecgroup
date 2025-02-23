1219bdbf8b9749319c5e0fd776de84bba709bbf8
Mark Balajadia <markb.freelance@gmail.com>
2025-02-23 18:18:16 +0800

    scrn-1900 about height adjusted

--- src/app/home/sections/about.tsx
+++ src/app/home/sections/about.tsx
@@ -11,7 +11,7 @@ const About: React.FC = () => {
   });
 
   return (
-    <div className="w-full bg-transparent flex flex-col md:flex-row px-12 outline-dashed scrn-1900:h-220 scrn-2k:h-320">
+    <div className="w-full bg-transparent flex flex-col md:flex-row px-12 outline-dashed scrn-1900:h-256 scrn-2k:h-320">
       {/* Left Image Section */}
       <div className="w-full hidden md:block md:w-2/5 xl:w-1/2">
         <Image
