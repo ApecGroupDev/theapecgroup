168fa41fd5fd29fb82cbba39d5007cd28e275239
Mark Balajadia <markb.freelance@gmail.com>
2025-02-23 16:42:20 +0800

    scrn-1900 featureboxes config

--- src/app/home/sections/featureboxes.tsx
+++ src/app/home/sections/featureboxes.tsx
@@ -46,34 +46,34 @@ const FeatureBoxes: React.FC = () => {
   });
 
   return (
-    <div className="bg-transparent md:px-12 w-900:px-20 scrn-2k:h-384">
+    <div className="bg-transparent md:px-12 w-900:px-20 scrn-1900:px-40 scrn-1900:h-256 scrn-2k:h-384">
       {/* Section Title */}
       <div className="text-center">
         <motion.p
           ref={ref} // Set the ref to the element
-          className="text-5xl md:text-5xl w-900:text-5xl lg:text-5xl xl:text-6xl mac-14:text-7xl mac-16:text-7xl 2xl:text-8xl scrn-2k:text-9xl font-extrabold text-gray-800 mb-4"
+          className="text-5xl md:text-5xl w-900:text-5xl lg:text-5xl xl:text-6xl mac-14:text-7xl mac-16:text-7xl scrn-1900:text-8xl scrn-2k:text-9xl font-extrabold text-gray-800"
           initial={{ y: 70, opacity: 0 }}
           animate={{ y: inView ? 0 : 50, opacity: inView ? 1 : 0 }}
           transition={{ duration: 0.5, ease: "easeOut" }}
         >
           WHY CHOOSE
         </motion.p>
-        <p className="text-5xl md:text-5xl w-900:text-5xl lg:text-5xl xl:text-6xl mac-14:text-7xl mac-16:text-7xl 2xl:text-8xl scrn-2k:text-9xl font-extrabold text-gray-800 mb-4">
+        <p className="text-5xl md:text-5xl w-900:text-5xl lg:text-5xl xl:text-6xl mac-14:text-7xl mac-16:text-7xl scrn-1900:text-8xl scrn-2k:text-9xl font-extrabold text-gray-800 mb-4">
           <span className="text-red-600"> APEC?</span>
         </p>
       </div>
 
       {/* Feature Boxes */}
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 lg:gap-12 gap-8 mac-16:mt-12 2xl:mt-16">
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 mac-16:mt-12 scrn-1900:mt-12 scrn-1900:gap-y-24">
         {/* First Row: 3 Boxes */}
         {features.slice(0, 3).map((feature, index) => (
           <div
             key={index}
-            className="relative mx-auto group bg-gray-100 p-6 rounded-lg shadow-lg hover:shadow-xl hover:bg-white transition-all w-3/5 md:w-10/12 lg:w-11/12 xl:w-10/12 mac-14:w-11/12 2xl:w-9/12 scrn-2k:w-10/12 duration-500"
+            className="relative mx-auto group bg-gray-100 p-6 rounded-lg shadow-lg hover:shadow-xl hover:bg-white transition-all md:w-10/12 lg:w-11/12 xl:w-10/12 mac-14:w-11/12 scrn-1900:w-116 scrn-2k:w-10/12 duration-500"
           >
-            <div className="flex flex-col items-center justify-center h-40 md:h-24 w-900:h-28 lg:h-40 xl:h-52 mac-14:h-60 mac-16:h-72 2xl:h-80 scrn-2k:h-96">
+            <div className="flex flex-col items-center justify-center h-40 md:h-24 w-900:h-28 lg:h-40 xl:h-52 mac-14:h-60 mac-16:h-72 scrn-1900:h-72 scrn-2k:h-96">
               {feature.icon}
-              <h3 className="md:text-sm lg:text-xl 2xl:text-2xl font-bold text-center mt-4 text-gray-800">{feature.title}</h3>
+              <h3 className="md:text-sm lg:text-xl scrn-1900:text-2xl font-bold text-center mt-4 text-gray-800">{feature.title}</h3>
             </div>
 
             {/* Description (Hidden by default, revealed on hover) */}
@@ -86,13 +86,13 @@ const FeatureBoxes: React.FC = () => {
         ))}
 
         {/* Second Row: 2 Boxes (Centered) */}
-        <div className="flex flex-wrap justify-center col-span-1 md:col-span-3 gap-8 md:gap-14 xl:gap-24 mac-14:gap-32 mac-16:gap-24 2xl:col-span-3 2xl:gap-44 scrn-2k:mt-12">
+        <div className="flex flex-wrap justify-center col-span-1 md:col-span-3 gap-8 md:gap-14 mac-14:gap-32 mac-16:gap-24 scrn-1900:gap-28 scrn-2k:mt-12">
           {features.slice(3).map((feature, index) => (
             <div
               key={index}
-              className="relative group bg-gray-100 p-6 rounded-lg shadow-lg hover:shadow-xl hover:bg-white transition-all duration-500 w-3/5 md:w-1/4 lg:w-3/12"
+              className="relative group bg-gray-100 p-6 rounded-lg shadow-lg hover:shadow-xl hover:bg-white transition-all duration-500 w-3/5 md:w-1/4 lg:w-3/12 scrn-1900:w-116"
             >
-              <div className="flex flex-col items-center justify-center h-40 md:h-24 w-900:h-32 lg:h-40 xl:h-52 mac-14:h-60 mac-16:h-72 2xl:h-80 scrn-2k:h-96">
+              <div className="flex flex-col items-center justify-center h-40 md:h-24 w-900:h-32 lg:h-40 xl:h-52 mac-14:h-60 mac-16:h-72 scrn-1900:h-72 scrn-2k:h-96">
                 {feature.icon}
                 <h3 className="md:text-sm lg:text-xl xl:text-2xl font-bold text-center mt-4 text-gray-800">{feature.title}</h3>
               </div>
