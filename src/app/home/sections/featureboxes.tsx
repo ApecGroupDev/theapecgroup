de54cafa4c412c5e32b330f621b777bccb97e8c5
Mark Balajadia <markb.freelance@gmail.com>
2025-02-24 01:49:01 +0800

    scrn-2k featureboxes config

--- src/app/home/sections/featureboxes.tsx
+++ src/app/home/sections/featureboxes.tsx
@@ -46,7 +46,7 @@ const FeatureBoxes: React.FC = () => {
   });
 
   return (
-    <div className="bg-transparent md:px-12 w-900:px-20 scrn-1900:px-40 scrn-1900:h-304 scrn-2k:h-384">
+    <div className="bg-transparent md:px-12 w-900:px-20 scrn-1900:px-40 scrn-1900:h-304 scrn-2k:px-72 scrn-2k:h-416">
       {/* Section Title */}
       <div className="text-center">
         <motion.p
@@ -64,12 +64,12 @@ const FeatureBoxes: React.FC = () => {
       </div>
 
       {/* Feature Boxes */}
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 mac-16:mt-12 scrn-1900:mt-12 scrn-1900:gap-y-24">
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 mac-16:mt-12 scrn-1900:mt-12 scrn-1900:gap-y-24 scrn-2k:gap-y-28">
         {/* First Row: 3 Boxes */}
         {features.slice(0, 3).map((feature, index) => (
           <div
             key={index}
-            className="relative mx-auto group bg-gray-100 p-6 rounded-lg shadow-lg hover:shadow-xl hover:bg-white transition-all md:w-10/12 lg:w-11/12 xl:w-10/12 mac-14:w-11/12 scrn-1900:w-116 scrn-2k:w-10/12 duration-500"
+            className="relative mx-auto group bg-gray-100 p-6 rounded-lg shadow-lg hover:shadow-xl hover:bg-white transition-all md:w-10/12 lg:w-11/12 xl:w-10/12 mac-14:w-11/12 scrn-1900:w-116 scrn-2k:w-128 duration-500"
           >
             <div className="flex flex-col items-center justify-center h-40 md:h-24 w-900:h-28 lg:h-40 xl:h-52 mac-14:h-60 mac-16:h-72 scrn-1900:h-72 scrn-2k:h-96">
               {feature.icon}
@@ -86,11 +86,11 @@ const FeatureBoxes: React.FC = () => {
         ))}
 
         {/* Second Row: 2 Boxes (Centered) */}
-        <div className="flex flex-wrap justify-center col-span-1 md:col-span-3 gap-8 md:gap-14 mac-14:gap-32 mac-16:gap-24 scrn-1900:gap-28 scrn-2k:mt-12">
+        <div className="flex flex-wrap justify-center col-span-1 md:col-span-3 gap-8 md:gap-14 mac-14:gap-32 mac-16:gap-24 scrn-1900:gap-28 scrn-2k:gap-36">
           {features.slice(3).map((feature, index) => (
             <div
               key={index}
-              className="relative group bg-gray-100 p-6 rounded-lg shadow-lg hover:shadow-xl hover:bg-white transition-all duration-500 w-3/5 md:w-1/4 lg:w-3/12 scrn-1900:w-116"
+              className="relative group bg-gray-100 p-6 rounded-lg shadow-lg hover:shadow-xl hover:bg-white transition-all duration-500 w-3/5 md:w-1/4 lg:w-3/12 scrn-1900:w-116 scrn-2k:w-128"
             >
               <div className="flex flex-col items-center justify-center h-40 md:h-24 w-900:h-32 lg:h-40 xl:h-52 mac-14:h-60 mac-16:h-72 scrn-1900:h-72 scrn-2k:h-96">
                 {feature.icon}
