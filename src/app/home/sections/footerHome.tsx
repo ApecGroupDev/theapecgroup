49ebeb778c5983cc34a6cd31fd1e439dc9a31259
Mark Balajadia <markb.freelance@gmail.com>
2025-02-26 14:45:23 +0800

    lg footer config

--- src/app/home/sections/footerHome.tsx
+++ src/app/home/sections/footerHome.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="text-black font-inter md:px-8 pt-32 md:pt-4 lg:pt-6 xl:pt-24 scrn-1500:pt-40 scrn-1700:pt-52 scrn-1700:px-20 scrn-1900:pt-32 scrn-1900:px-24 scrn-2k:px-32">
+    <footer className="text-black font-inter md:px-8 pt-32 md:pt-4 xl:pt-24 scrn-1500:pt-40 scrn-1700:pt-52 scrn-1700:px-20 scrn-1900:pt-32 scrn-1900:px-24 scrn-2k:px-32">
       <div className="grid md:grid-cols-3 gap-8 items-center text-gray-300">
         {/* Column 1: Social Media Icons */}
         <div className="flex flex-col items-center md:items-start space-y-2 md:space-y-1 lg:space-y-2">
