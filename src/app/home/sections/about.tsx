da51d1867ef45cab3131d28abeeb71e5632ef817
Mark Balajadia <markb.freelance@gmail.com>
2025-02-27 04:21:44 +0800

    scrn-900 about config

--- src/app/home/sections/about.tsx
+++ src/app/home/sections/about.tsx
@@ -11,7 +11,7 @@ const About: React.FC = () => {
   });
 
   return (
-    <div className="w-full bg-transparent flex flex-col md:flex-row px-12 outline-dashed lg:h-140 xl:h-184 scrn-1500:-mt-8 scrn-1500:h-224 scrn-1700:h-240 scrn-1900:h-256 scrn-2k:h-352">
+    <div className="w-full bg-transparent flex flex-col md:flex-row px-12 outline-dashed scrn-900:h-128 lg:h-140 xl:h-184 scrn-1500:-mt-8 scrn-1500:h-224 scrn-1700:h-240 scrn-1900:h-256 scrn-2k:h-352">
       {/* Left Image Section */}
       <div className="w-full hidden md:block md:w-2/5 xl:w-1/2">
         <Image
@@ -41,12 +41,12 @@ const About: React.FC = () => {
         <p className="text-2xl md:text-xl lg:text-2xl scrn-1500:text-3xl scrn-1700:text-3xl scrn-1900:text-4xl scrn-2k:text-5xl font-medium text-gray-800">
           Petroleum Equipment Services
         </p>
-        <p className="mt-4 scrn-1900:mt-12 text-justify md:text-sm w-900:text-base lg:text-base xl:text-lg scrn-1500:text-xl scrn-1700:text-2xl scrn-1900:text-2xl scrn-1900:tracking-widest scrn-2k:text-3xl text-gray-800">
+        <p className="mt-4 scrn-1900:mt-12 text-justify md:text-sm scrn-900:text-base lg:text-base xl:text-lg scrn-1500:text-xl scrn-1700:text-2xl scrn-1900:text-2xl scrn-1900:tracking-widest scrn-2k:text-3xl text-gray-800">
           The APEC group you know today started out originally as Atlanta Petroleum Equipment Company in September of 1989.
           From our modest beginning as a small service company, we have grown to a leader in our industry offering a full
           line of petroleum equipment, installation, repair and support services.
         </p>
-        <p className="mt-4 md:text-sm text-justify w-900:text-base lg:text-base xl:text-lg scrn-1500:text-xl scrn-1700:text-2xl scrn-1900:text-2xl scrn-1900:tracking-widest scrn-2k:text-3xl text-gray-800">
+        <p className="mt-4 md:text-sm text-justify scrn-900:text-base lg:text-base xl:text-lg scrn-1500:text-xl scrn-1700:text-2xl scrn-1900:text-2xl scrn-1900:tracking-widest scrn-2k:text-3xl text-gray-800">
           APEC is committed to providing only the highest quality products and services available. Our industry continues
           to evolve technically year after year. APEC is committed to staying on the leading edge of all the latest innovations
           and technologies to not only support our customer’s needs but so we can also be the industry experts you can rely on.
