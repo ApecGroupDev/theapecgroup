a8568103007a47d96a716cdac90ca7ed93e3a844
Mark Balajadia <markb.freelance@gmail.com>
2025-02-27 03:43:37 +0800

    scrn-900 hero config

--- src/app/home/sections/hero.tsx
+++ src/app/home/sections/hero.tsx
@@ -3,16 +3,16 @@ import Image from 'next/image';
 
 const Hero: React.FC = () => {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-between bg-transparent md:px-12 lg:h-144 xl:h-160 scrn-1500:h-198 scrn-1700:h-224 scrn-1900:h-240 scrn-2k:h-320">
+    <div className="flex flex-col md:flex-row items-center justify-between bg-transparent md:px-12 scrn-900:h-124 lg:h-144 xl:h-160 scrn-1500:h-198 scrn-1700:h-224 scrn-1900:h-240 scrn-2k:h-320">
       {/* Left Text Section */}
-      <div className="-space-y-1.5 text-center md:text-left md:pb-0 w-800:pb-10 w-900:pb-8 lg:pb-5">
-        <p className="text-3xl md:text-xl w-900:text-2xl lg:text-3xl scrn-1500:text-4xl scrn-1700:text-4xl scrn-1900:text-5xl scrn-2k:text-6xl font-bold whitespace-nowrap">
+      <div className="-space-y-1.5 text-center md:text-left md:pb-0 w-800:pb-10 lg:pb-5">
+        <p className="text-3xl md:text-xl scrn-900:text-2xl lg:text-3xl scrn-1500:text-4xl scrn-1700:text-4xl scrn-1900:text-5xl scrn-2k:text-6xl font-bold whitespace-nowrap">
           YOUR TOP CHOICE
         </p>
-        <p className="text-4xl md:text-3xl w-900:text-4xl lg:text-5xl scrn-1500:text-6xl scrn-1700:text-6xl scrn-1900:text-7xl scrn-2k:text-8xl font-regular italic text-red-500 tracking-wider whitespace-nowrap">
+        <p className="text-4xl md:text-3xl scrn-900:text-4xl lg:text-5xl scrn-1500:text-6xl scrn-1700:text-6xl scrn-1900:text-7xl scrn-2k:text-8xl font-regular italic text-red-500 tracking-wider whitespace-nowrap">
           ONE-STOP SHOP
         </p>
-        <p className="text-3xl md:text-xl w-900:text-2xl lg:text-3xl scrn-1500:text-4xl scrn-1700:text-4xl scrn-1900:text-5xl scrn-2k:text-6xl font-regular">
+        <p className="text-3xl md:text-xl scrn-900:text-2xl lg:text-3xl scrn-1500:text-4xl scrn-1700:text-4xl scrn-1900:text-5xl scrn-2k:text-6xl font-regular">
           ATLANTA COMPANY
         </p>
       </div>
