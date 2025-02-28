18313856f8b1d3ffbb653296d9881792996cabcd
Mark Balajadia <markb.freelance@gmail.com>
2025-02-28 10:43:40 +0800

    sm hero config

--- src/app/home/sections/hero.tsx
+++ src/app/home/sections/hero.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image';
 
 const Hero: React.FC = () => {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-between bg-transparent md:px-12 md:h-116 scrn-800:h-120 scrn-900:h-124 lg:h-144 xl:h-160 scrn-1500:h-198 scrn-1700:h-224 scrn-1900:h-240 scrn-2k:h-320">
+    <div className="flex flex-col-reverse sm:flex-row sm:ms-2 md:ms-0 items-center justify-between bg-transparent sm:h-124 md:px-12 md:h-116 scrn-800:h-120 scrn-900:h-124 lg:h-144 xl:h-160 scrn-1500:h-198 scrn-1700:h-224 scrn-1900:h-240 scrn-2k:h-320">
       {/* Left Text Section */}
-      <div className="-space-y-1.5 text-center md:text-left lg:pb-5">
+      <div className="-space-y-1.5 text-center sm:text-left lg:pb-5">
         <p className="text-3xl md:text-xl scrn-900:text-2xl lg:text-3xl scrn-1500:text-4xl scrn-1700:text-4xl scrn-1900:text-5xl scrn-2k:text-6xl font-bold whitespace-nowrap">
           YOUR TOP CHOICE
         </p>
