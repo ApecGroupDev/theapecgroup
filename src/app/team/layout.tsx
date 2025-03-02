7f266c82b787f91af275e395114f8407293d9636
Mark Balajadia <markb.freelance@gmail.com>
2025-03-03 05:32:32 +0800

    our team base page / layout

--- src/app/team/layout.tsx
+++ src/app/team/layout.tsx
@@ -5,20 +5,17 @@ export default function TeamLayout({ children }: { children: React.ReactNode }) 
   return (
     <div
       style={{
-        backgroundImage: "",
-        backgroundSize: "cover",
-        backgroundPosition: "center top",
-        backgroundRepeat: "no-repeat",
-        minHeight: "100vh",
-
+        backgroundImage: "url('/backgrounds/OurTeam_Bg.jpg')",
+        backgroundSize: 'cover', // Cover the entire container
+        backgroundPosition: 'center top', // Center horizontally, align top
+        backgroundRepeat: 'no-repeat',
+        minHeight: '100vh',
       }}
     >
 
       <Header />
 
       <div className="w-full">{children}</div>
-
-      <Footer />
 
     </div>
   )
