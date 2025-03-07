8a042070720580088add0760dfa12e1b573fa662
Mark Balajadia <markb.freelance@gmail.com>
2025-03-08 05:43:04 +0800

    scrn-1900 footer config

--- src/app/team/sections/footerTeam.tsx
+++ src/app/team/sections/footerTeam.tsx
@@ -5,12 +5,12 @@ import Image from 'next/image';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="text-black font-inter md:px-8 md:pt-4 lg:pt-6 xl:pt-24 mac-14:pt-40 mac-16:pt-52 mac-16:px-20 2xl:px-24 scrn-1900:pt-40 scrn-2k:px-32">
+    <footer className="text-black font-inter md:px-8 md:pt-4 lg:pt-6 xl:pt-24 mac-14:pt-40 mac-16:pt-52 mac-16:px-20 scrn-1900:px-24 scrn-1900:pt-40 scrn-2k:px-32">
       <div className="grid md:grid-cols-3 gap-8 items-center text-gray-300">
         {/* Column 1: Social Media Icons */}
         <div className="flex flex-col items-center md:items-start space-y-2 md:space-y-1 lg:space-y-2">
-          <span className="scrn-mobile:text-xs sm:text-xs md:text-xs md:pt-6 xl:text-sm mac-14:text-base mac-16:text-lg 2xl:text-lg footer-text-dark">For More Information</span>
-          <span className='md:ps-0 lg:ps-6 scrn-mobile:text-xs sm:text-xs md:text-xs xl:text-sm mac-14:text-base mac-16:text-lg 2xl:text-lg whitespace-nowrap footer-text-dark'>855-444-APEC | Sales@TheAPECgroup.com</span>
+          <span className="scrn-mobile:text-xs sm:text-xs md:text-xs md:pt-6 xl:text-sm mac-14:text-base mac-16:text-lg scrn-1900:text-lg footer-text-dark">For More Information</span>
+          <span className='md:ps-0 lg:ps-6 scrn-mobile:text-xs sm:text-xs md:text-xs xl:text-sm mac-14:text-base mac-16:text-lg scrn-1900:text-lg whitespace-nowrap footer-text-dark'>855-444-APEC | Sales@TheAPECgroup.com</span>
           <div className="flex  space-x-4 md:ps-0 lg:ps-12">
             <SocialIcon
               url="https://twitter.com"
@@ -45,17 +45,17 @@ const Footer: React.FC = () => {
 
         {/* Column 2: Addresses and Contact Information */}
         <div className="flex flex-col space-y-4 pb-4 items-center md:pt-2">
-          <p className="hidden md:block md:text-xs xl:text-sm mac-14:text-base mac-16:text-lg 2xl:text-lg scrn-2k:text-xl leading-relaxed footer-text-dark">
+          <p className="hidden md:block md:text-xs xl:text-sm mac-14:text-base mac-16:text-lg scrn-1900:text-lg scrn-2k:text-xl leading-relaxed footer-text-dark">
             Main Office: Atlanta, Georgia<br />
             4732-E North Royal Atlanta<br />
             Drive Tucker, GA 30084
           </p>
-          <p className="hidden md:block md:text-xs xl:text-sm mac-14:text-base mac-16:text-lg 2xl:text-lg scrn-2k:text-xl leading-relaxed footer-text-dark">
+          <p className="hidden md:block md:text-xs xl:text-sm mac-14:text-base mac-16:text-lg scrn-1900:text-lg scrn-2k:text-xl leading-relaxed footer-text-dark">
             Branch Office: Houston, Texas<br />
             505 Garden Oaks Boulevard<br />
             Houston, TX 77018
           </p>
-          <p className="hidden md:hidden lg:block md:text-xs xl:text-sm mac-14:text-base mac-16:text-lg 2xl:text-lg scrn-2k:text-xl leading-relaxed footer-text-dark">
+          <p className="hidden md:hidden lg:block md:text-xs xl:text-sm mac-14:text-base mac-16:text-lg scrn-1900:text-lg scrn-2k:text-xl leading-relaxed footer-text-dark">
             For More Info: 855-444-APEC<br />
             <a
               href="mailto:Service@TheAPECgroup.com"
@@ -81,7 +81,7 @@ const Footer: React.FC = () => {
               alt="APEC Since 1989"
               height={1206}
               width={1315}
-              className="transition-transform duration-300 ease-in-out transform hover:scale-105 h-20 sm:h-16 md:h-28 lg:h-40 xl:h-52 mac-14:h-60 mac-16:h-72 2xl:h-72 scrn-2k:h-80 w-auto"
+              className="transition-transform duration-300 ease-in-out transform hover:scale-105 h-20 sm:h-16 md:h-28 lg:h-40 xl:h-52 mac-14:h-60 mac-16:h-72 scrn-1900:h-72 scrn-2k:h-80 w-auto"
             />
           </Link>
         </div>
@@ -91,7 +91,7 @@ const Footer: React.FC = () => {
 
       {/* Copyright*/}
       <div className="w-full py-4">
-        <p className="text-xs md:text-xs xl:text-sm mac-14:text-base mac-16:text-lg 2xl:text-lg scrn-2k:text-xl text-center text-gray-300 font-light">
+        <p className="text-xs md:text-xs xl:text-sm mac-14:text-base mac-16:text-lg scrn-1900:text-lg scrn-2k:text-xl text-center text-gray-300 font-light">
           Copyright 2024–2025 © The APEC Group. All rights reserved.
         </p>
       </div>
