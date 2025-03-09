198ae365e2b98b68da648c28f9d7650b2ec9bde6
Mark Balajadia <markb.freelance@gmail.com>
2025-03-10 04:06:04 +0800

    scrn-900 teamcard component config

--- src/app/team/components/teamCard.tsx
+++ src/app/team/components/teamCard.tsx
@@ -8,16 +8,16 @@ type TeamCardProps = {
 
 const TeamCard: React.FC<TeamCardProps> = ({ name, title }) => {
   return (
-    <div className="relative flex items-center rounded-lg overflow-visible outline-dashed outline-green-600 lg:w-72 lg:h-28 xl:w-80 xl:h-32 scrn-1500:w-112 scrn-1500:h-36 scrn-1700:w-116 w-128 h-48">
+    <div className="relative flex items-center rounded-lg overflow-visible outline-dashed outline-green-600 scrn-900:w-60 scrn-900:h-24 lg:w-72 lg:h-28 xl:w-80 xl:h-32 scrn-1500:w-112 scrn-1500:h-36 scrn-1700:w-116 w-128 h-48">
 
       {/* Text content */}
       <div className="z-10 flex-1 text-end">
         <h2 className="text-red-600 font-bold xl:text-lg scrn-1500:text-xl 2xl:text-xl">{name}</h2>
-        <p className="text-gray-600 lg:text-sm xl:text-base scrn-1500:text-lg 2xl:text-lg">{title}</p>
+        <p className="text-gray-600 scrn-900:text-xs lg:text-sm xl:text-base scrn-1500:text-lg 2xl:text-lg">{title}</p>
       </div>
 
-      <div className="relative w-56 h-48 lg:w-36 lg:h-28 xl:w-40 xl:h-32 scrn-1500:w-44 scrn-1500:h-36 ps-1 bg-gradient-to-r from-red-600 to-transparent clip-parallelogram overflow-visible">
-        <div className="absolute w-56 h-48 lg:w-36 lg:h-28 xl:w-40 xl:h-32 scrn-1500:w-44 scrn-1500:h-36 bg-white clip-parallelogram flex items-center justify-center">
+      <div className="relative w-56 h-48 scrn-900:w-32 scrn-900:h-24 lg:w-36 lg:h-28 xl:w-40 xl:h-32 scrn-1500:w-44 scrn-1500:h-36 ps-1 bg-gradient-to-r from-red-600 to-transparent clip-parallelogram overflow-visible">
+        <div className="absolute w-56 h-48 scrn-900:w-32 scrn-900:h-24 lg:w-36 lg:h-28 xl:w-40 xl:h-32 scrn-1500:w-44 scrn-1500:h-36 bg-white clip-parallelogram flex items-center justify-center">
           <Image
             src="/placeholders/profile.png" 
             alt={`${name}'s profile`} 
