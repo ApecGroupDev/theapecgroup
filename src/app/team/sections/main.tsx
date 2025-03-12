23392b500de90da8d6e0b450d828b450df2f2c78
Mark Balajadia <markb.freelance@gmail.com>
2025-03-12 14:36:46 +0800

    adjustmed mx on all screens - grid cols 2

--- src/app/team/sections/main.tsx
+++ src/app/team/sections/main.tsx
@@ -39,12 +39,12 @@ const TeamPage: React.FC = () => {
       <span className="scrn-mobile:text-sm sm:text-base scrn-900:text-xl lg:text-2xl xl:text-2xl scrn-1500:text-4xl 2xl:text-4xl font-bold text-gray-800">Dedicated professionals delivering exceptional service</span><br />
       <span className="scrn-900:text-xl lg:text-2xl xl:text-2xl scrn-1500:text-4xl 2xl:text-4xl font-bold text-red-600">100% privately owned and operated</span>
 
-      <div className="scrn-300:hidden scrn-mobile:hidden sm:space-y-5 md:space-y-6 scrn-800:space-y-7 scrn-900:space-y-7 lg:space-y-8 xl:space-y-10 scrn-1500:space-y-10 scrn-1700:space-y-8 scrn-1900:space-y-16 scrn-2k:space-y-32 sm:mt-6 md:mt-4 scrn-800:mt-4 scrn-900:mt-4 lg:mt-6 xl:mt-8 scrn-1500:mt-16 2xl:mt-16">
+      <div className="scrn-300:hidden scrn-mobile:hidden sm:block sm:space-y-5 md:space-y-6 scrn-800:space-y-7 scrn-900:space-y-7 lg:space-y-8 xl:space-y-10 scrn-1500:space-y-10 scrn-1700:space-y-8 scrn-1900:space-y-16 scrn-2k:space-y-32 sm:mt-6 md:mt-4 scrn-800:mt-4 scrn-900:mt-4 lg:mt-6 xl:mt-8 scrn-1500:mt-16 2xl:mt-16">
         {rows.map((row, rowIndex) => (
           <div
             key={rowIndex}
             className={`grid justify-items-center sm:mx-6 md:mx-4 scrn-800:mx-4 scrn-900:mx-12 lg:mx-8 xl:mx-20 scrn-1500:mx-20 scrn-1700:mx-24 scrn-1900:mx-0 scrn-2k:mx-24 
-              ${row.length === 2 ? 'grid-cols-2 sm:mx-28 md:mx-32 scrn-800:mx-36 scrn-900:mx-48 lg:mx-44 xl:mx-64 scrn-1500:mx-72 scrn-1700:mx-80 scrn-1900:mx-80 scrn-2k:mx-132' : 'grid-cols-3'
+              ${row.length === 2 ? 'grid-cols-2 sm:mx-28 md:mx-32 scrn-800:mx-40 scrn-900:mx-48 lg:mx-44 xl:mx-64 scrn-1500:mx-72 scrn-1700:mx-96 scrn-1900:mx-80 scrn-2k:mx-132' : 'grid-cols-3'
               }`}
           >
             {row.map((member) => (
