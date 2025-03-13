31c4da9fe0e8befc03e653ef79178fffd833b6f7
Mark Balajadia <markb.freelance@gmail.com>
2025-03-13 14:07:43 +0800

    homepage scrn-300 hero config

--- src/app/home/sections/hero.tsx
+++ src/app/home/sections/hero.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image';
 
 const Hero: React.FC = () => {
   return (
-    <div className="flex flex-col-reverse sm:flex-row sm:ms-2 md:ms-0 items-center justify-between bg-transparent scrn-mobile:h-136 sm:h-124 md:px-12 md:h-116 scrn-800:h-120 scrn-900:h-124 lg:h-144 xl:h-160 scrn-1500:h-198 scrn-1700:h-224 scrn-1900:h-240 scrn-2k:h-320">
+    <div className="flex flex-col-reverse sm:flex-row sm:ms-2 md:ms-0 items-center justify-between bg-transparent scrn-300:h-128 scrn-mobile:h-136 sm:h-124 md:px-12 md:h-116 scrn-800:h-120 scrn-900:h-124 lg:h-144 xl:h-160 scrn-1500:h-198 scrn-1700:h-224 scrn-1900:h-240 scrn-2k:h-320">
       {/* Left Text Section */}
-      <div className="-space-y-1.5 text-center sm:text-left lg:pb-5">
+      <div className="-space-y-1.5 text-center scrn-300:mt-6 sm:text-left lg:pb-5">
         <p className="text-2xl md:text-xl scrn-900:text-2xl lg:text-3xl scrn-1500:text-4xl scrn-1700:text-4xl scrn-1900:text-5xl scrn-2k:text-6xl font-bold whitespace-nowrap">
           YOUR TOP CHOICE
         </p>
@@ -18,13 +18,13 @@ const Hero: React.FC = () => {
       </div>
 
       {/* Right Image Section */}
-      <div className="w-full md:w-1/2 flex justify-center scrn-mobile:mt-auto">
+      <div className="w-full md:w-1/2 flex justify-center scrn-300:mt-auto scrn-mobile:mt-auto">
         <Image
           src="/products/Fuel_Dispenser_Face_Left.png"
           alt="Hero"
           width={1011}
           height={1211}
-          className="rounded-lg object-cover scrn-mobile:w-72 scrn-mobile:mb-6 w-10/12"
+          className="rounded-lg object-cover scrn-300:w-64 scrn-mobile:w-72 scrn-mobile:mb-6 w-10/12"
         />
       </div>
     </div>
