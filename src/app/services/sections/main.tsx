fb266e7ecc8a7b200a8a8f26818192a3d2ce0569
Mark Balajadia <markb.freelance@gmail.com>
2025-03-16 05:23:37 +0800

    lg main content config

--- src/app/services/sections/main.tsx
+++ src/app/services/sections/main.tsx
@@ -43,23 +43,23 @@ const features: Services[] = [
 
 const MainServices: React.FC = () => {
   return (
-    <div className="bg-transparent outline-dashed outline-green-500 xl:px-12 scrn-1500:px-20 scrn-1700:px-20 scrn-1900:px-28 xl:h-240 scrn-1500:h-288 scrn-1700:h-320 scrn-1900:h-352 scrn-2k:h-448">
+    <div className="bg-transparent outline-dashed outline-green-500 lg:px-16 xl:px-12 scrn-1500:px-20 scrn-1700:px-20 scrn-1900:px-28 lg:h-208 xl:h-240 scrn-1500:h-288 scrn-1700:h-320 scrn-1900:h-352 scrn-2k:h-448">
       {/* Section Title */}
-      <div className="text-center xl:py-12 scrn-1500:py-16 scrn-1700:py-16 scrn-1900:py-28 scrn-2k:py-32">
-        <div className="xl:text-2xl scrn-1500:text-4xl scrn-1700:text-4xl scrn-1900:text-5xl font-semibold text-gray-800">
+      <div className="text-center lg:py-8 xl:py-12 scrn-1500:py-16 scrn-1700:py-16 scrn-1900:py-28 scrn-2k:py-32">
+        <div className="lg:text-xl xl:text-2xl scrn-1500:text-4xl scrn-1700:text-4xl scrn-1900:text-5xl font-semibold text-gray-800">
           Check out some of our services here!
         </div>
       </div>
 
       {/* Feature Boxes */}
-      <div className="grid grid-cols-3 xl:gap-y-16 scrn-1500:gap-y-16 scrn-1700:gap-y-24 scrn-1900:gap-y-32 scrn-2k:gap-y-44">
+      <div className="grid grid-cols-3 lg:gap-y-10 xl:gap-y-16 scrn-1500:gap-y-16 scrn-1700:gap-y-24 scrn-1900:gap-y-32 scrn-2k:gap-y-44">
         {features.map((feature, index) => (
           <div
             key={index}
-            className="relative mx-auto group bg-gray-100 bg-opacity-60 p-6 rounded-lg shadow-lg hover:shadow-xl hover:bg-white transition-all xl:w-80 scrn-1500:w-112 scrn-1700:w-116 scrn-1900:w-116 scrn-2k:w-140 duration-500"
+            className="relative mx-auto group bg-gray-100 bg-opacity-60 p-6 rounded-lg shadow-lg hover:shadow-xl hover:bg-white transition-all lg:w-64 xl:w-80 scrn-1500:w-112 scrn-1700:w-116 scrn-1900:w-116 scrn-2k:w-140 duration-500"
           >
             {/* Image Display */}
-            <div className="flex flex-col items-center justify-center xl:h-64 scrn-1500:h-72 scrn-1700:h-80 scrn-1900:h-80 scrn-2k:h-116">
+            <div className="flex flex-col items-center justify-center lg:h-48 xl:h-64 scrn-1500:h-72 scrn-1700:h-80 scrn-1900:h-80 scrn-2k:h-116">
               <img
                 src={feature.imageSrc}
                 alt={feature.title}
