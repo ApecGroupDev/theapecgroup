5924557bd0a0a663da75b8d498dcd571c22c1474
Mark Balajadia <markb.freelance@gmail.com>
2025-03-20 09:43:42 +0800

    removed comments on hero imaging and canopies

--- src/app/about/sections/hero.tsx
+++ src/app/about/sections/hero.tsx
@@ -22,31 +22,29 @@ const Hero: React.FC = () => {
 
   return (
     <div id="hero-section" className="scrn-300:h-124 scrn-mobile:h-136 sm:h-120 md:h-116 scrn-800:h-116 scrn-900:h-124 lg:h-132 scrn-1100:h-160 xl:h-176 scrn-1500:h-198 scrn-1700:h-224 scrn-1900:h-240 scrn-2k:h-320 sm:justify-center justify-between overflow-hidden">
-      {/* Background Image */}
+
       <div className="absolute inset-0 z-10 sm:mt-24 md:mt-0 scrn-300:hidden scrn-mobile:hidden">
         <Image
           src="/backgrounds/aboutUs/Hero.png"
           alt="Background"
-          style={{objectFit: 'cover'}}
+          style={{ objectFit: 'cover' }}
           width={2786}
           height={1438}
           priority
         />
       </div>
 
-      {/* Background Image Mobile */}
       <div className="absolute inset-0 z-10 sm:hidden">
         <Image
           src="/backgrounds/aboutUs/Hero-Mobile.png"
           alt="Background"
-          style={{objectFit: 'cover'}}
+          style={{ objectFit: 'cover' }}
           width={2786}
           height={1438}
           priority
         />
       </div>
 
-      {/* Left Text Section */}
       <div className={`fixed scrn-300:p-2 scrn-mobile:p-4 sm:p-8 md:ms-0 scrn-300:space-y-4 sm:space-y-8 md:space-y-0 xl:space-y-4 scrn-1500:space-y-4 scrn-1700:space-y-12 scrn-1900:space-y-4 scrn-2k:space-y-24 text-left md:p-12 scrn-900:my-8 lg:my-16 xl:my-24 scrn-1900:my-44 scrn-2k:my-40 transition-opacity duration-100 ${fadeOut ? 'opacity-0 pointer-events-none' : 'opacity-100'
         }`}
       >
