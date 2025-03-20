dc28bce8f3278bee29fd970bd1aaafa151679f67
Mark Balajadia <markb.freelance@gmail.com>
2025-03-20 09:57:58 +0800

    try fix

--- src/app/services/imaging-and-canopies/page.tsx
+++ src/app/services/imaging-and-canopies/page.tsx
@@ -1,13 +1,9 @@
 import Footer from "./sections/footerImaging";
-import Hero from "./sections/hero";
 import MainImaging from "./sections/main";
 
 const Services: React.FC = () => {
   return (
     <div className='flex flex-col'>
-      <div>
-        <Hero />
-      </div>
 
       <div>
         <MainImaging />
