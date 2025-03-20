6392be7f5eb1ce4f3a7d31d2e349f842f01cecc3
Mark Balajadia <markb.freelance@gmail.com>
2025-03-20 09:40:32 +0800

    last deploy try fix

--- src/app/about/sections/hero.tsx
+++ src/app/about/sections/hero.tsx
@@ -12,7 +12,7 @@ const Hero: React.FC = () => {
       const heroSection = document.getElementById('hero-section');
       if (heroSection) {
         const { top } = heroSection.getBoundingClientRect();
-        setFadeOut(top < -80); // Adjust to control when the fade starts
+        setFadeOut(top < -80);
       }
     };
 
@@ -50,7 +50,6 @@ const Hero: React.FC = () => {
       <div className={`fixed scrn-300:p-2 scrn-mobile:p-4 sm:p-8 md:ms-0 scrn-300:space-y-4 sm:space-y-8 md:space-y-0 xl:space-y-4 scrn-1500:space-y-4 scrn-1700:space-y-12 scrn-1900:space-y-4 scrn-2k:space-y-24 text-left md:p-12 scrn-900:my-8 lg:my-16 xl:my-24 scrn-1900:my-44 scrn-2k:my-40 transition-opacity duration-100 ${fadeOut ? 'opacity-0 pointer-events-none' : 'opacity-100'
         }`}
       >
-        {/* Breadcrumb */}
         <nav className="pt-14 scrn-300:mt-20 scrn-mobile:mt-28">
           <ul className="flex items-center space-x-2 scrn-1500:text-2xl scrn-1700:text-2xl scrn-1900:text-xl scrn-2k:text-3xl font-semibold text-gray-600 tracking-widest">
             <li>
@@ -63,7 +62,6 @@ const Hero: React.FC = () => {
           </ul>
         </nav>
 
-        {/* Headline */}
         <div>
           <div className="scrn-mobile:-space-y-3 sm:-space-y-3 md:space-y-0 scrn-800:-space-y-4 scrn-900:-space-y-6 lg:-space-y-4 xl:-space-y-6 scrn-1500:-space-y-8 scrn-1700:-space-y-8 scrn-1900:-space-y-12">
             <p className="text-7xl sm:text-8xl md:text-8xl scrn-800:text-9xl scrn-900:text-9xl lg:text-9xl xl:text-10xl scrn-1500:text-12xl scrn-1700:text-13xl scrn-1900:text-14xl scrn-2k:text-18xl scale-y-110 text-red-600 p-0 m-0 font-semibold whitespace-nowrap">
