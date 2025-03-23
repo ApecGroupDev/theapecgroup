ac5fbd40c69021c6e5409b35501fbdffea207d4a
Mark Balajadia <markb.freelance@gmail.com>
2025-03-23 08:19:31 +0800

    scrn-mobile hero config

--- src/app/services/imaging-and-canopies/sections/hero.tsx
+++ src/app/services/imaging-and-canopies/sections/hero.tsx
@@ -23,7 +23,7 @@ const Hero: React.FC = () => {
   return (
     <div
       id="hero-section"
-      className=" relative pointer-events-none scrn-300:h-112 scrn-mobile:h-112 sm:h-112 md:h-116 scrn-800:h-120 scrn-900:h-128 lg:h-136 scrn-1100:h-160 xl:h-176 scrn-1500:h-208 scrn-1700:h-240 scrn-1900:h-256 scrn-2k:h-352 sm:justify-center justify-between overflow-hidden"
+      className=" relative pointer-events-none scrn-300:h-112 scrn-mobile:h-136 sm:h-112 md:h-116 scrn-800:h-120 scrn-900:h-128 lg:h-136 scrn-1100:h-160 xl:h-176 scrn-1500:h-208 scrn-1700:h-240 scrn-1900:h-256 scrn-2k:h-352 sm:justify-center justify-between overflow-hidden"
     >
       <div className="absolute sm:inset-0 z-10 scrn-300:bottom-20 scrn-300:scale-150 scrn-mobile:bottom-16 scrn-mobile:scale-150 sm:mt-12 md:mt-0">
         <Image
@@ -37,11 +37,11 @@ const Hero: React.FC = () => {
       </div>
 
       <div
-        className={`fixed scrn-300:p-2 scrn-mobile:p-4 scrn-300:space-y-8 scrn-mobile:space-y-10 sm:p-8 md:ms-0 sm:space-y-5 md:space-y-12 scrn-800:space-y-12 scrn-800:tracking-tighter scrn-900:space-y-12 lg:space-y-6 xl:space-y-6 scrn-1500:space-y-28 scrn-1700:space-y-36 scrn-1900:space-y-20 scrn-2k:space-y-40 text-left md:p-12 scrn-900:my-8 lg:my-16 xl:my-24 scrn-1900:my-48 scrn-2k:my-64 transition-opacity duration-100 ${fadeOut ? 'opacity-0 pointer-events-none' : 'opacity-100'
+        className={`fixed scrn-300:p-2 scrn-mobile:p-4 scrn-300:space-y-8 scrn-mobile:space-y-16 sm:p-8 md:ms-0 sm:space-y-5 md:space-y-12 scrn-800:space-y-12 scrn-800:tracking-tighter scrn-900:space-y-12 lg:space-y-6 xl:space-y-6 scrn-1500:space-y-28 scrn-1700:space-y-36 scrn-1900:space-y-20 scrn-2k:space-y-40 text-left md:p-12 scrn-mobile:my-12 scrn-900:my-8 lg:my-16 xl:my-24 scrn-1900:my-48 scrn-2k:my-64 transition-opacity duration-100 ${fadeOut ? 'opacity-0 pointer-events-none' : 'opacity-100'
           }`}
       >
         <nav className="pt-14 scrn-300:mt-6 scrn-mobile:mt-6">
-          <ul className="flex sm:items-center space-x-2 sm:text-2xs md:text-base scrn-800:text-sm scrn-900:text-sm scrn-1500:text-2xl scrn-1700:text-2xl scrn-1900:text-xl scrn-2k:text-3xl font-semibold text-gray-600 tracking-widest">
+          <ul className="flex sm:items-center space-x-2 scrn-mobile:text-2xs sm:text-2xs md:text-base scrn-800:text-sm scrn-900:text-sm scrn-1500:text-2xl scrn-1700:text-2xl scrn-1900:text-xl scrn-2k:text-3xl font-semibold text-gray-600 tracking-widest">
             <li>
               <Link href="/home">
                 Home
@@ -55,7 +55,7 @@ const Hero: React.FC = () => {
         </nav>
 
         <div>
-          <span className="scrn-300:text-6xl scrn-mobile:text-7xl sm:text-6xl md:text-6xl scrn-800:text-7xl scrn-900:text-7xl scrn-900:tracking-wide lg:text-8xl lg:tracking-tighter xl:text-9xl xl:tracking-tighter scrn-1500:text-9xl scrn-1500:tracking-normal scrn-1700:text-10xl scrn-1700:tracking-normal scrn-1900:text-11xl scrn-1900:tracking-tighter scrn-2k:text-13xl scale-y-110 text-white p-0 m-0 font-semibold whitespace-nowrap">
+          <span className="scrn-300:text-6xl scrn-mobile:text-6xl sm:text-6xl md:text-6xl scrn-800:text-7xl scrn-900:text-7xl scrn-900:tracking-wide lg:text-8xl lg:tracking-tighter xl:text-9xl xl:tracking-tighter scrn-1500:text-9xl scrn-1500:tracking-normal scrn-1700:text-10xl scrn-1700:tracking-normal scrn-1900:text-11xl scrn-1900:tracking-tighter scrn-2k:text-13xl scale-y-110 text-white p-0 m-0 font-semibold whitespace-nowrap">
             IMAGING & <br />
             CANOPIES
           </span>
