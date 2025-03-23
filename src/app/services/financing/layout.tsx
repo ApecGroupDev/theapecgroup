5d2727014dff807d3e918bb17d69d0caef027511
Mark Balajadia <markb.freelance@gmail.com>
2025-03-23 19:25:00 +0800

    financing layout.tsx file

---
+++ src/app/services/financing/layout.tsx
@@ -0,0 +1,20 @@
+import Header from "@/components/header";
+
+export default function ServicesLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div
+      style={{
+        backgroundImage: "url('/backgrounds/services/financing/Background_Financing.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center top",
+        backgroundRepeat: "no-repeat",
+        minHeight: "100vh",
+      }}>
+
+      <Header />
+
+      <div className="w-full">{children}</div>
+
+    </div>
+  );
+}
