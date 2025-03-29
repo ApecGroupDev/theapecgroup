a50d283d78b439b47b1e7c27484210771c2aa29b
Mark Balajadia <markb.freelance@gmail.com>
2025-03-30 04:17:36 +0800

    construction layout config

---
+++ src/app/services/construction/layout.tsx
@@ -0,0 +1,20 @@
+import Header from "@/components/header";
+
+export default function ServicesLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div
+      style={{
+        backgroundImage: "url('/backgrounds/services/construction/Background_Construction.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center top",
+        backgroundRepeat: "no-repeat",
+        minHeight: "100vh",
+      }}>
+
+      <Header />
+
+      <div className="w-full">{children}</div>
+
+    </div>
+  );
+}
