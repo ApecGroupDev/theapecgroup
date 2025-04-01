1761383d4f72d2db6b0d7af2ed8d55d1b5a7c850
Mark Balajadia <markb.freelance@gmail.com>
2025-04-01 16:56:43 +0800

    compliance hero config

--- src/app/services/compliance/sections/hero.tsx
+++ src/app/services/compliance/sections/hero.tsx
@@ -44,7 +44,7 @@ const Hero: React.FC = () => {
       </div>
 
       <div
-        className={`fixed scrn-300:p-2 scrn-mobile:p-4 scrn-300:space-y-14 scrn-mobile:space-y-14 sm:p-8 md:ms-0 sm:space-y-10 md:space-y-8 scrn-800:space-y-7 scrn-800:tracking-tighter scrn-900:space-y-12 lg:space-y-9 xl:space-y-14 scrn-1500:space-y-12 scrn-1700:space-y-20 scrn-1900:space-y-28 scrn-2k:space-y-32 text-left md:p-12 scrn-1900:ps-16 scrn-300:my-12 scrn-mobile:my-12 sm:my-12 md:my-6 scrn-800:my-6 scrn-900:my-8 lg:my-16 xl:my-28 scrn-1500:my-40 scrn-1700:my-48 scrn-1900:my-52 scrn-2k:my-80 transition-opacity duration-100 ${fadeOut ? 'opacity-0 pointer-events-none' : 'opacity-100'
+        className={`fixed scrn-300:p-2 scrn-mobile:p-4 scrn-300:space-y-14 scrn-mobile:space-y-14 sm:p-8 md:ms-0 sm:space-y-10 md:space-y-8 scrn-800:space-y-7 scrn-800:tracking-tighter scrn-900:space-y-12 lg:space-y-6 xl:space-y-24 scrn-1500:space-y-24 scrn-1700:space-y-12 scrn-1900:space-y-28 scrn-2k:space-y-32 text-left md:p-12 scrn-1900:ps-16 scrn-300:my-12 scrn-mobile:my-12 sm:my-12 md:my-6 scrn-800:my-6 scrn-900:my-8 lg:my-16 xl:my-28 scrn-1500:my-40 scrn-1700:my-48 scrn-1900:my-52 scrn-2k:my-80 transition-opacity duration-100 ${fadeOut ? 'opacity-0 pointer-events-none' : 'opacity-100'
           }`}
       >
         <nav className="pt-14 scrn-300:mt-6 scrn-mobile:mt-6">
@@ -57,13 +57,13 @@ const Hero: React.FC = () => {
             <li>/</li>
             <li>Services</li>
             <li>/</li>
-            <li className="text-red-600">APEC Construction</li>
+            <li className="text-red-600">APEC Compliance</li>
           </ul>
         </nav>
 
         <div>
           <span className="font-semibold whitespace-nowrap text-red-600">
-            <span className='scrn-300:text-6xl scrn-mobile:text-7xl sm:text-5xl md:text-6xl scrn-800:text-7xl scrn-900:text-8xl lg:text-8xl xl:text-9xl scrn-1500:text-9xl scrn-1700:text-10xl scrn-1900:text-9xl scrn-1900:tracking-tighter scrn-2k:text-10xl scrn-2k:tracking-normal'>
+            <span className='scrn-300:text-3xl scrn-mobile:text-4xl sm:text-4xl md:text-5xl scrn-800:text-5xl scrn-800:tracking-normal scrn-900:text-6xl lg:text-7xl lg:tracking-tighter xl:text-7xl xl:tracking-wide scrn-1500:text-8xl scrn-1500:tracking-tight scrn-1700:text-9xl scrn-1700:tracking-tighter scrn-1900:text-9xl scrn-1900:tracking-tighter scrn-2k:text-10xl scrn-2k:tracking-normal'>
               INSPECTION AND <br />
               COMPLIANCE
             </span>
