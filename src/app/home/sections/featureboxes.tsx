b9f7aef30dab0bd5d1ad9a5b73e83b6b821eb68b
Mark Balajadia <markb.freelance@gmail.com>
2025-04-04 00:27:23 +0800

    featureboxes scrn-1750 config

--- src/app/home/sections/featureboxes.tsx
+++ src/app/home/sections/featureboxes.tsx
@@ -64,6 +64,7 @@ const FeatureBoxes: React.FC = () => {
       scrn-1500:px-52 scrn-1500:h-256 
       scrn-1650:h-272
       scrn-1700:px-48 scrn-1700:h-288 
+      scrn-1750:h-304
       scrn-1900:px-40 scrn-1900:h-304 
       scrn-2k:px-72 scrn-2k:h-416"
     >
