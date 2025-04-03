41960843ff1b85f3b34ca3c0ada7a80f749ed2a2
Mark Balajadia <markb.freelance@gmail.com>
2025-04-03 08:52:02 +0800

    homepage hero scrn-1350 config

--- src/app/home/sections/hero.tsx
+++ src/app/home/sections/hero.tsx
@@ -15,6 +15,7 @@ const Hero: React.FC = () => {
       scrn-1200:h-160 
       xl:h-160 
       scrn-1300:h-176
+      scrn-1350:h-184
       scrn-1500:h-198 
       scrn-1700:h-224 
       scrn-1900:h-240 
