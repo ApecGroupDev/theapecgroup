62754f8394d436650092c2a892dd8eeb3f42a0a6
Mark Balajadia <markb.freelance@gmail.com>
2025-04-04 07:11:04 +0800

    hero scrn-2500 config

--- src/app/home/sections/hero.tsx
+++ src/app/home/sections/hero.tsx
@@ -39,7 +39,8 @@ const Hero: React.FC = () => {
       scrn-2300:h-312
       scrn-2350:h-316
       scrn-2400:h-320
-      scrn-2450:h-324"
+      scrn-2450:h-324
+      scrn-2500:h-326"
     >
       {/* Left Text Section */}
       <div className="-space-y-1.5 text-center scrn-300:mt-6 sm:text-left lg:pb-5">
