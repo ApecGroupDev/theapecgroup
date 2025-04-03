17b5c2b5816b029fe5df8177d27719c1d2ba180e
Mark Balajadia <markb.freelance@gmail.com>
2025-04-04 02:16:41 +0800

    membership assoc scrn-1950 config

--- src/app/home/sections/membershiplogoslider.tsx
+++ src/app/home/sections/membershiplogoslider.tsx
@@ -49,6 +49,7 @@ const MembershipLogosCarousel: React.FC = () => {
       scrn-1850:h-240
       scrn-1900:h-288
       scrn-1901:h-272 
+      scrn-1950:h-264
       scrn-2k:h-384"
     >
       <div className="flex flex-col scrn-mobile:justify-center md:justify-center items-center scrn-900:pb-14 lg:pb-24 xl:pb-0 h-full w-full scrn-2k:gap-y-16">
