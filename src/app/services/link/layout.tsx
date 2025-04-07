cfcc415d541660ee26019a4c8a8ffcaf4c5c2f19
Mark Balajadia <markb.freelance@gmail.com>
2025-04-07 16:52:00 +0800

    new code structure for layout and page tsx files service link

--- src/app/services/link/layout.tsx
+++ src/app/services/link/layout.tsx
@@ -1,18 +1,7 @@
-import Header from "@/components/header";
-
-export default function ServicesLayout({ children }: { children: React.ReactNode }) {
+export default function ServicesLinkLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div
-      style={{
-        backgroundImage: "url('/backgrounds/services/link/Background_Link.jpg')",
-        backgroundSize: "cover",
-        backgroundPosition: "center top",
-        backgroundRepeat: "no-repeat",
-        minHeight: "100vh",
-      }}>
-
+    <div className="bg-[url('/backgrounds/services/link/Background_Link.jpg')] bg-cover bg-top bg-no-repeat min-h-screen">
       <div className="w-full">{children}</div>
-
     </div>
   );
 }
--- src/app/services/link/page.tsx
+++ src/app/services/link/page.tsx
@@ -1,24 +1,14 @@
 import Footer from "./sections/footerLink";
 import MainLink from "./sections/main";
 import Hero from "./sections/hero";
-const Services: React.FC = () => {
+const ServicesLink: React.FC = () => {
   return (
     <div className='flex flex-col'>
-
-      <div>
-        <Hero />
-      </div>
-
-      <div>
-        <MainLink />
-      </div>
-
-      <div>
-        <Footer />
-      </div>
-
+      <Hero />
+      <MainLink />
+      <Footer />
     </div>
   );
 };
 
-export default Services;
+export default ServicesLink;
