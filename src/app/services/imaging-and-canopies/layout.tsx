1d58b90b5fee0c41cb88dbc1bf8f99cddd46ed75
Mark Balajadia <markb.freelance@gmail.com>
2025-04-08 05:25:35 +0800

    services imaging update on layout and page tsx files

--- src/app/services/imaging-and-canopies/layout.tsx
+++ src/app/services/imaging-and-canopies/layout.tsx
@@ -1,20 +1,7 @@
-import Header from "@/components/header";
-
-export default function ServicesLayout({ children }: { children: React.ReactNode }) {
+export default function ServicesFinancingLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div
-      style={{
-        backgroundImage: "url('/backgrounds/services/imaging-and-canopies/Background_Imaging.jpg')",
-        backgroundSize: "cover",
-        backgroundPosition: "center top",
-        backgroundRepeat: "no-repeat",
-        minHeight: "100vh",
-      }}>
-
-      <Header />
-
+    <div className="bg-[url('/backgrounds/services/imaging-and-canopies/Background_Imaging.jpg')] bg-cover bg-top bg-no-repeat min-h-screen">
       <div className="w-full">{children}</div>
-
     </div>
   );
 }
--- src/app/services/imaging-and-canopies/page.tsx
+++ src/app/services/imaging-and-canopies/page.tsx
@@ -2,24 +2,14 @@ import Footer from "./sections/footerImaging";
 import MainImaging from "./sections/main";
 import Hero from "./sections/hero";
 
-const Services: React.FC = () => {
+const ServicesImaging: React.FC = () => {
   return (
     <div className='flex flex-col'>
-
-      <div>
         <Hero />
-      </div>
-
-      <div>
         <MainImaging />
-      </div>
-
-      <div>
         <Footer />
-      </div>
-
     </div>
   );
 };
 
-export default Services;
+export default ServicesImaging;
