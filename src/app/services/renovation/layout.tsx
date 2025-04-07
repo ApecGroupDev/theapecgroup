e506585ff3aac1cd9cdaf6f8fa91579ec6c4d14d
Mark Balajadia <markb.freelance@gmail.com>
2025-04-08 05:49:29 +0800

    last deploy error fix

--- src/app/services/renovation/layout.tsx
+++ src/app/services/renovation/layout.tsx
@@ -1,5 +1,3 @@
-import Header from "@/components/header";
-
 export default function ServicesRenovationLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="bg-[url('/backgrounds/services/renovation/Background_Renovation.jpg')] bg-cover bg-top bg-no-repeat min-h-screen">
