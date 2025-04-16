b92583d663e852cd22d7a441bbd8fdfaf46f3dd9
Mark Balajadia <markb.freelance@gmail.com>
2025-04-17 02:42:58 +0800

    our team page files updated to new config

--- src/app/team/sections/hero.tsx
+++ src/app/team/sections/hero.tsx
@@ -30,10 +30,29 @@ const Hero: React.FC = () => {
   return (
     <div
       id="hero-section"
-      className=" relative pointer-events-none scrn-300:h-116 scrn-mobile:h-116 sm:h-112 md:h-120 scrn-800:h-120 scrn-900:h-128 lg:h-136 scrn-1100:h-160 xl:h-176 scrn-1500:h-208 scrn-1700:h-240 scrn-1900:h-256 scrn-2k:h-352 sm:justify-center justify-between overflow-hidden"
+      className=" relative pointer-events-none scrn-600:justify-center justify-between overflow-hidden
+      scrn-300:h-116 
+      scrn-450:h-116 
+      scrn-600:h-112 
+      scrn-750:h-120 
+      scrn-800:h-120 
+      scrn-900:h-128 
+      scrn-1000:h-136 
+      scrn-1100:h-160 
+      scrn-1250:h-176 
+      scrn-1500:h-208 
+      scrn-1700:h-240
+      scrn-1900:h-256 
+      scrn-2500:h-352"
     >
       {/* Background Image */}
-      <div className="absolute inset-0 z-10 sm:mt-6 md:mt-4 lg:mt-9 xl:mt-9 scrn-1500:mt-0 2xl:mt-0 scrn-300:hidden scrn-mobile:hidden">
+      <div className="absolute inset-0 z-10 hidden scrn-500:block
+        scrn-600:mt-6 
+        scrn-750:mt-4 
+        scrn-1000:mt-9 
+        scrn-1250:mt-9 
+        scrn-1500:mt-0"
+      >
         <Image
           src="/backgrounds/ourTeam/Hero.png"
           alt="Background"
@@ -45,7 +64,7 @@ const Hero: React.FC = () => {
       </div>
 
       {/* Background Image Mobile */}
-      <div className="absolute inset-0 z-10 sm:hidden">
+      <div className="absolute inset-0 z-10 scrn-600:hidden">
         <Image
           src="/backgrounds/ourTeam/Hero-Mobile.png"
           alt="Background"
@@ -58,12 +77,53 @@ const Hero: React.FC = () => {
 
       {/* Left Text Section */}
       <div
-        className={`fixed scrn-300:p-2 scrn-mobile:p-4 scrn-300:space-y-8 scrn-mobile:space-y-10 sm:p-8 md:ms-0 sm:space-y-5 md:space-y-0 scrn-800:space-y-4 scrn-900:space-y-4 lg:space- xl:space-y-6 scrn-1500:space-y-6 scrn-1700:space-y-20 scrn-1900:space-y-10 scrn-2k:space-y-32 text-left md:p-12 scrn-900:my-8 lg:my-16 xl:my-24 scrn-1900:my-40 scrn-2k:my-36 transition-opacity duration-250 ${fadeOut ? 'opacity-0 pointer-events-none' : 'opacity-100'
+        className={`fixed text-left 
+          scrn-300:p-2 
+          scrn-450:p-4 
+          scrn-600:p-8
+          scrn-750:p-12
+
+          scrn-750:ms-0
+
+          scrn-300:space-y-8 
+          scrn-450:space-y-10  
+          scrn-600:space-y-5
+          scrn-750:space-y-0 
+          scrn-800:space-y-4 
+          scrn-900:space-y-4 
+          scrn-1000:space- 
+          scrn-1250:space-y-6 
+          scrn-1500:space-y-6 
+          scrn-1700:space-y-20 
+          scrn-1900:space-y-10 
+          scrn-2500:space-y-32  
+
+          scrn-900:my-8 
+          scrn-1000:my-16 
+          scrn-1250:my-24 
+          scrn-1900:my-40 
+          scrn-2500:my-36
+          transition-opacity duration-250 ${fadeOut ? 'opacity-0 pointer-events-none' : 'opacity-100'
           }`}
       >
         {/* Breadcrumb */}
-        <nav className="pt-14 scrn-300:mt-6 scrn-mobile:mt-6">
-          <ul className="flex sm:items-center space-x-2 scrn-300:text-2xs scrn-mobile:text-2xs sm:text-3xs md:text-3xs scrn-800:text-2xs scrn-900:text-2xs lg:text-2xs xl:text-xs scrn-1500:text-md scrn-1700:text-md scrn-1900:text-xl scrn-2k:text-xl font-semibold text-gray-600 tracking-widest">
+        <nav className="pt-14 scrn-300:mt-6 scrn-450:mt-6">
+          <ul className="flex scrn-600:items-center text-gray-600 tracking-widest
+            space-x-2 
+            scrn-300:text-2xs 
+            scrn-450:text-2xs 
+            scrn-600:text-3xs 
+            scrn-750:text-3xs 
+            scrn-800:text-2xs 
+            scrn-900:text-2xs 
+            scrn-1000:text-2xs 
+            scrn-1250:text-xs
+            scrn-1500:text-md 
+            scrn-1700:text-md 
+            scrn-1900:text-xl 
+            scrn-2500:text-xl 
+            font-semibold"
+          >
             <li>
               <Link href="/home" className="hover:text-red-600">
                 Home
@@ -75,11 +135,37 @@ const Hero: React.FC = () => {
         </nav>
 
         {/* Headline */}
-        <div className='scrn-300:space-x-2 xl:space-x-4 scrn-1500:space-x-0 2xl:space-x-0'>
-          <span className="scrn-300:text-5xl scrn-mobile:text-5xl sm:text-7xl md:text-7xl scrn-800:text-7xl scrn-900:text-8xl lg:text-8xl xl:text-9xl scrn-1500:text-10xl scrn-1700:text-10xl scrn-1900:text-10xl scrn-2k:text-12xl font-regular italic whitespace-nowrap">
+        <div className='scrn-300:space-x-2 scrn-1250:space-x-4 scrn-1500:space-x-0'>
+          <span className="
+            scrn-300:text-5xl 
+            scrn-450:text-5xl 
+            scrn-600:text-7xl 
+            scrn-750:text-7xl 
+            scrn-800:text-7xl 
+            scrn-900:text-8xl 
+            scrn-1000:text-8xl 
+            scrn-1250:text-9xl 
+            scrn-1500:text-10xl 
+            scrn-1700:text-10xl 
+            scrn-1900:text-10xl 
+            scrn-2500:text-12xl"
+          >
             OUR
           </span>
-          <span className="scrn-300:text-6xl scrn-mobile:text-7xl sm:text-8xl md:text-9xl scrn-800:text-9xl scrn-900:text-9xl lg:text-10xl xl:text-11xl scrn-1500:text-12xl scrn-1700:text-13xl scrn-1900:text-14xl scrn-2k:text-18xl scale-y-110 text-red-600 p-0 m-0 font-semibold whitespace-nowrap">
+          <span className="scale-y-110 text-red-600 p-0 m-0 font-semibold whitespace-nowrap
+            scrn-300:text-6xl 
+            scrn-450:text-7xl 
+            scrn-600:text-8xl 
+            scrn-750:text-9xl 
+            scrn-800:text-9xl 
+            scrn-900:text-9xl 
+            scrn-1000:text-10xl 
+            scrn-1250:text-11xl 
+            scrn-1500:text-12xl 
+            scrn-1700:text-13xl 
+            scrn-1900:text-14xl 
+            scrn-2500:text-18xl"
+          >
             TEAM
           </span>
         </div>
--- src/app/team/sections/main.tsx
+++ src/app/team/sections/main.tsx
@@ -35,16 +35,83 @@ const TeamPage: React.FC = () => {
   }
 
   return (
-    <section className="outline-dashed outline-green-500 text-center scrn-mobile:pt-6 sm:pt-6 md:pt-4 scrn-800:pt-4 scrn-900:pt-4 lg:pt-12 xl:pt-16 scrn-1500:pt-24 2xl:pt-24 scrn-300:h-198 scrn-mobile:h-182 sm:h-160 md:h-198 scrn-800:h-208 scrn-900:h-240 lg:h-288 xl:h-352 scrn-1500:h-384 scrn-1700:h-448 scrn-1900:h-480 scrn-2k:h-640">
-      <span className="scrn-300:text-xs scrn-mobile:text-sm sm:text-base scrn-900:text-xl lg:text-2xl xl:text-2xl scrn-1500:text-4xl 2xl:text-4xl font-bold text-gray-800">Dedicated professionals delivering exceptional service</span><br />
-      <span className="scrn-900:text-xl lg:text-2xl xl:text-2xl scrn-1500:text-4xl 2xl:text-4xl font-bold text-red-600">100% privately owned and operated</span>
+    <section className="outline-dashed outline-green-500 text-center font-bold text-gray-800
+      scrn-450:pt-6 
+      scrn-600:pt-6 
+      scrn-750:pt-4 
+      scrn-800:pt-4 
+      scrn-900:pt-4 
+      scrn-1000:pt-12 
+      scrn-1250:pt-16 
+      scrn-1500:pt-24 
+       
+      scrn-300:h-198 
+      scrn-450:h-182 
+      scrn-600:h-160 
+      scrn-750:h-198 
+      scrn-800:h-208 
+      scrn-900:h-240 
+      scrn-1000:h-288 
+      scrn-1250:h-352 
+      scrn-1500:h-384
+      scrn-1700:h-448 
+      scrn-1900:h-480 
+      scrn-2500:h-640"
+    >
+      <span className="
+        scrn-300:text-xs 
+        scrn-450:text-sm 
+        scrn-600:text-base 
+        scrn-900:text-xl 
+        scrn-1000:text-2xl 
+        scrn-1250:text-2xl 
+        scrn-1500:text-4xl"
+      >
+        Dedicated professionals delivering exceptional service
+      </span><br />
+      <span className="font-bold text-red-600
+        scrn-900:text-xl 
+        scrn-1000:text-2xl 
+        scrn-1250:text-2xl 
+        scrn-1500:text-4xl"
+      >
+        100% privately owned and operated
+      </span>
 
-      <div className="scrn-300:hidden scrn-mobile:hidden sm:block sm:space-y-5 md:space-y-6 scrn-800:space-y-7 scrn-900:space-y-7 lg:space-y-8 xl:space-y-10 scrn-1500:space-y-10 scrn-1700:space-y-8 scrn-1900:space-y-16 scrn-2k:space-y-32 sm:mt-6 md:mt-4 scrn-800:mt-4 scrn-900:mt-4 lg:mt-6 xl:mt-8 scrn-1500:mt-16 2xl:mt-16">
+      <div className="scrn-300:hidden scrn-450:hidden scrn-600:block 
+        scrn-600:space-y-5 
+        scrn-750:space-y-6 
+        scrn-800:space-y-7 
+        scrn-900:space-y-7 
+        scrn-1000:space-y-8 
+        scrn-1250:space-y-10 
+        scrn-1500:space-y-10 
+        scrn-1700:space-y-8 
+        scrn-1900:space-y-16 
+        scrn-2500:space-y-32 
+        scrn-600:mt-6 
+        scrn-750:mt-4 
+        scrn-800:mt-4 
+        scrn-900:mt-4 
+        scrn-1000:mt-6 
+        scrn-1250:mt-8 
+        scrn-1500:mt-16"
+      >
         {rows.map((row, rowIndex) => (
           <div
             key={rowIndex}
-            className={`grid justify-items-center sm:mx-6 md:mx-4 scrn-800:mx-4 scrn-900:mx-12 lg:mx-8 xl:mx-20 scrn-1500:mx-20 scrn-1700:mx-24 scrn-1900:mx-0 scrn-2k:mx-24 
-              ${row.length === 2 ? 'grid-cols-2 sm:mx-auto sm:max-w-116 md:mx-auto md:max-w-128 scrn-800:mx-auto scrn-800:max-w-128 scrn-900:mx-auto scrn-900:max-w-144 lg:mx-auto lg:max-w-168 xl:mx-auto xl:max-w-192 scrn-1500:mx-auto scrn-1500:max-w-224 scrn-1700:mx-auto scrn-1700:max-w-256 scrn-1900:mx-auto scrn-1900:max-w-320 scrn-2k:mx-auto scrn-2k:max-w-384' : 'grid-cols-3'
+            className={`grid justify-items-center 
+              scrn-600:mx-6 
+              scrn-750:mx-4 
+              scrn-800:mx-4 
+              scrn-900:mx-12 
+              scrn-1000:mx-8 
+              scrn-1250:mx-20 
+              scrn-1500:mx-20 
+              scrn-1700:mx-24 
+              scrn-1900:mx-0 
+              scrn-2500:mx-24 
+              ${row.length === 2 ? 'grid-cols-2 scrn-600:mx-auto scrn-600:max-w-116 scrn-750:mx-auto scrn-750:max-w-128 scrn-800:mx-auto scrn-800:max-w-128 scrn-900:mx-auto scrn-900:max-w-144 scrn-1000:mx-auto scrn-1000:max-w-168 scrn-1250:mx-auto scrn-1250:max-w-192 scrn-1500:mx-auto scrn-1500:max-w-224 scrn-1700:mx-auto scrn-1700:max-w-256 scrn-1900:mx-auto scrn-1900:max-w-320 scrn-2500:mx-auto scrn-2500:max-w-384' : 'grid-cols-3'
               }`}
           >
             {row.map((member) => (
@@ -54,7 +121,7 @@ const TeamPage: React.FC = () => {
         ))}
       </div>
 
-      <div className="space-y-4 mx-4 sm:hidden pt-6">
+      <div className="space-y-4 mx-4 scrn-600:hidden pt-6">
         {Array.from({ length: Math.ceil(teamMembers.length / 2) }).map((_, rowIndex) => {
           const start = rowIndex * 2;
           const row = teamMembers.slice(start, start + 2); // Get 2 members per row
@@ -63,8 +130,8 @@ const TeamPage: React.FC = () => {
             <div
               key={rowIndex}
               className={`grid ${row.length === 1 && start + 1 === teamMembers.length
-                  ? 'grid-cols-1 justify-items-center'
-                  : 'grid-cols-2 justify-items-center'
+                ? 'grid-cols-1 justify-items-center'
+                : 'grid-cols-2 justify-items-center'
                 } gap-4`}
             >
               {row.map((member, index) => (
