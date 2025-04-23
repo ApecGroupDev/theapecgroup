26f3e86ea0a855b4f66123bc84597c1659132e32
Mark Balajadia <markb.freelance@gmail.com>
2025-04-24 06:31:25 +0800

    checkmark badge component update

--- src/components/checkmarkBadgeSmall.tsx
+++ src/components/checkmarkBadgeSmall.tsx
@@ -1,6 +1,6 @@
 import { FaCheck } from "react-icons/fa";
 
-const CheckmarkBadgeSmall = ({ text = "", size = 20, fontSize = 12, bgColor = "red", textColor = "white" }) => {
+const CheckmarkBadgeSmall = ({ text = "", size = 16, fontSize = 9, bgColor = "red", textColor = "white" }) => {
   return (
     <div style={{ display: "flex", alignItems: "center" }}>
       <div style={{
