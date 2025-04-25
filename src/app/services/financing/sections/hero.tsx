5b643ad473a0511b9557424492e4d8b27acadc26
Mark Balajadia <markb.freelance@gmail.com>
2025-04-26 05:58:25 +0800

    services sub page financing dev on all screens

--- src/app/services/financing/sections/hero.tsx
+++ src/app/services/financing/sections/hero.tsx
@@ -30,9 +30,54 @@ const Hero: React.FC = () => {
   return (
     <div
       id="hero-section"
-      className=" relative pointer-events-none scrn-300:h-128 scrn-mobile:h-136 sm:h-112 md:h-116 scrn-800:h-120 scrn-900:h-128 lg:h-136 scrn-1100:h-160 xl:h-176 scrn-1500:h-208 scrn-1700:h-240 scrn-1900:h-256 scrn-2k:h-352 sm:justify-center justify-between overflow-hidden"
+      className="relative flex pointer-events-none items-center overflow-hidden
+      h-116
+      scrn-400:h-112
+      scrn-450:h-118
+      scrn-500:h-118
+      scrn-550:h-118
+      scrn-600:h-120
+      scrn-650:h-120
+      scrn-700:h-120
+      scrn-750:h-120
+      scrn-800:h-124
+      scrn-850:h-124
+      scrn-900:h-128
+      scrn-950:h-132
+      scrn-1000:h-140
+      scrn-1050:h-144
+      scrn-1100:h-148
+      scrn-1150:h-156
+      scrn-1200:h-164
+      scrn-1250:h-168
+      scrn-1300:h-176
+      scrn-1350:h-180
+      scrn-1400:h-188
+      scrn-1450:h-195
+      scrn-1500:h-198
+      scrn-1550:h-208
+      scrn-1600:h-216
+      scrn-1650:h-224
+      scrn-1700:h-228
+      scrn-1750:h-232
+      scrn-1800:h-240
+      scrn-1850:h-248
+      scrn-1900:h-256
+      scrn-1950:h-260
+      scrn-2000:h-268
+      scrn-2050:h-276
+      scrn-2100:h-280
+      scrn-2150:h-288
+      scrn-2200:h-296
+      scrn-2250:h-304
+      scrn-2300:h-308
+      scrn-2350:h-312
+      scrn-2400:h-316
+      scrn-2450:h-324
+      scrn-2500:h-326"
     >
-      <div className="absolute sm:inset-0 z-10 scrn-300:bottom-20 scrn-300:scale-150 scrn-mobile:bottom-16 scrn-mobile:scale-150 sm:mt-12 md:mt-0">
+      {/* Background Image */}
+      <div className="absolute z-10">
         <Image
           src="/backgrounds/services/financing/Hero_Financing.png"
           alt="Background"
@@ -40,15 +85,65 @@ const Hero: React.FC = () => {
           width={2786}
           height={1437}
           priority
+          className="
+          h-114
+          mb-4
+
+          scrn-400:h-96
+          scrn-400:mb-8
+          scrn-450:h-116
+          scrn-450:mb-6
+          scrn-500:h-120
+          scrn-500:mb-6
+          scrn-600:h-120
+          scrn-600:mt-4
+          scrn-700:h-124
+          scrn-700:mb-12
+          scrn-800:mb-0
+          scrn-800:mt-0
+          scrn-900:h-128
+          scrn-900:mb-12
+          scrn-900:mt-2
+          scrn-950:mt-8
+          scrn-1000:h-140
+          scrn-1000:mt-0
+          scrn-1000:mb-0
+          scrn-1100:h-152
+          scrn-1100:mt-4
+          scrn-1100:mb-8
+          scrn-1150:h-160
+          scrn-1150:mt-0
+          scrn-1150:mb-6
+          scrn-1200:h-172
+          scrn-1200:mb-0
+          scrn-1300:h-full
+          scrn-1300:mb-4
+          scrn-1300:mt-4"
         />
       </div>
 
-      <div
-        className={`fixed scrn-300:p-2 space-y-8 xl:space-y-12 scrn-mobile:p-4 sm:p-8 md:-ms-3 text-left md:p-12 scrn-800:-ms-3 scrn-900:-ms-3 scrn-1900:ps-24 scrn-300:my-20 scrn-mobile:my-32 sm:my-5 md:-my-5 scrn-800:-my-5 scrn-900:-my-2 lg:my-4 xl:my-6 scrn-1500:my-16 scrn-1700:my-16 scrn-1900:my-36 scrn-2k:my-60 transition-opacity duration-100 ${fadeOut ? 'opacity-0 pointer-events-none' : 'opacity-100'
-          }`}
+      {/* Left Text Section */}
+      <div className={`
+        fixed scrn-750:ms-0 p-2
+        scrn-600:ps-4 
+        scrn-800:ps-12
+        scrn-950:ps-16
+        scrn-1150:ps-20
+        scrn-1400:ps-28
+        scrn-2100:ps-32
+        scrn-2400:ps-40
+        text-left transition-opacity duration-100 ${fadeOut ? 'opacity-0 pointer-events-none' : 'opacity-100'
+        }`}
       >
-        <nav className="pt-14 scrn-300:mt-6 scrn-mobile:mt-6">
-          <ul className="flex sm:items-center space-x-2 scrn-300:text-2xs scrn-mobile:text-2xs sm:text-3xs md:text-3xs scrn-800:text-2xs scrn-900:text-2xs lg:text-2xs xl:text-xs scrn-1500:text-md scrn-1700:text-md scrn-1900:text-xl scrn-2k:text-xl font-semibold text-gray-600 tracking-widest">
+        {/* Breadcrumb */}
+        <nav className="">
+          <ul className="flex scrn-600:items-center space-x-2 font-semibold text-gray-600 tracking-widest
+            text-2xs 
+            scrn-600:text-xs
+            scrn-750:text-sm
+            scrn-1000:text-lg 
+            scrn-1500:text-xl"
+          >
             <li>
               <Link href="/home">
                 Home
@@ -57,13 +152,26 @@ const Hero: React.FC = () => {
             <li>/</li>
             <li>Services</li>
             <li>/</li>
-            <li className="text-red-600">Gas Station Financing Company</li>
+            <li className="text-red-600">APEC Financing</li>
           </ul>
         </nav>
 
         <div>
-          <span className="scrn-300:text-5xl scrn-mobile:text-5xl sm:text-3xl md:text-4xl scrn-800:text-4xl scrn-800:tracking-wider scrn-900:text-5xl scrn-900:tracking-tighter lg:text-5xl lg:tracking-normal xl:text-6xl xl:tracking-wide scrn-1500:text-7xl scrn-1500:tracking-wide scrn-1700:text-8xl scrn-1700:tracking-tighter scrn-1900:text-8xl scrn-1900:tracking-tighter scrn-2k:text-9xl scrn-2k:tracking-tight scale-y-110 p-0 m-0 font-semibold whitespace-nowrap">
-            GAS STATION <br className='sm:hidden' /> FINANCING <br />
+          <span className="font-semibold whitespace-nowrap
+            text-3xl 
+            scrn-350:text-4xl
+            scrn-450:text-4xl
+            scrn-600:text-3xl 
+            scrn-750:text-4xl 
+            scrn-800:text-4xl  
+            scrn-900:text-4xl 
+            scrn-1000:text-5xl 
+            scrn-1300:text-6xl 
+            scrn-1500:text-7xl 
+            scrn-1700:text-7xl  
+            scrn-2000:text-8xl"
+          >
+            GAS STATION <br className='scrn-600:hidden' /> FINANCING <br />
             COMPANY
           </span>
         </div>
