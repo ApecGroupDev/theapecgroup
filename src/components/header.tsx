0da1ed605ada1b4798d9b92afd5f3d4d56d4591d
Mark Balajadia <markb.freelance@gmail.com>
2025-05-08 03:44:10 +0800

    update on header services link dropdown menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -74,7 +74,7 @@ const Header: React.FC = () => {
         }`}
     >
       {/* Mobile Header */}
-      <div className="scrn-750:hidden container min-w-full relative flex items-center py-4">
+      <div className="scrn-1000:hidden container min-w-full relative flex items-center py-4">
         {/* Search Icon (Left) */}
         <button
           className="absolute mx-2 p-3 top-10 transform -translate-y-1/2 text-[#c62931] focus:outline-none z-20"
@@ -161,7 +161,7 @@ const Header: React.FC = () => {
       </div>
 
       {/* Desktop Header - 3 Grid Layout */}
-      <div className="hidden scrn-750:grid grid-cols-3 items-center px-6 py-1">
+      <div className="hidden scrn-1000:grid grid-cols-3 items-center px-6 py-1">
         {/* Left: Logo */}
         <div className="flex items-center">
           <Link href="/">
@@ -185,43 +185,65 @@ const Header: React.FC = () => {
                 onMouseEnter={() => setShowDropdown(true)}
                 onMouseLeave={() => setShowDropdown(false)}
               >
-                <Link
-                  href={path}
-                  className={`capitalize scrn-750:text-xs scrn-1000:text-base text-gray-900 hover:text-[#c62931] transition-colors duration-200 relative ${pathname.startsWith(path) ? "text-red-800" : ""
-                    }`}
-                >
-                  SERVICES
+                <div className="relative">
+                  <Link
+                    href={path}
+                    className={`capitalize scrn-750:text-xs scrn-1000:text-base text-gray-900 hover:text-[#c62931] transition-colors duration-200 relative ${pathname.startsWith(path) ? "text-red-800" : ""}`}
+                  >
+                    SERVICES
+                  </Link>
                   {pathname.startsWith(path) && (
                     <span className="absolute left-0 right-0 bottom-0 h-0.5 bg-[#c62931]" />
                   )}
-                </Link>
+                </div>
 
                 {/* DROPDOWN MENU */}
                 {showDropdown && (
                   <div
-                    className="absolute left-0 top-full w-40 bg-white shadow-lg rounded-md py-2 z-50"
+                    className="absolute left-0 top-full w-40 bg-transparent backdrop-blur-sm rounded-md py-2 z-50"
                   // Make sure there's no gap from the top of the dropdown to the bottom of the link
                   >
                     <Link
-                      href="/services/consulting"
-                      className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
+                      href="/services/compliance"
+                      className="block px-4 py-2 text-gray-700 hover:bg-[#c62931] hover:text-white rounded-md"
                       onClick={() => setShowDropdown(false)}
                     >
-                      Consulting
+                      Compliance
                     </Link>
                     <Link
-                      href="/services/development"
-                      className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
+                      href="/services/financing"
+                      className="block px-4 py-2 text-gray-700 hover:bg-[#c62931] hover:text-white rounded-md"
                       onClick={() => setShowDropdown(false)}
                     >
-                      Development
+                      Financing
                     </Link>
                     <Link
-                      href="/services/design"
-                      className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
+                      href="/services/imaging-and-canopies"
+                      className="block px-4 py-2 text-gray-700 hover:bg-[#c62931] hover:text-white rounded-md"
                       onClick={() => setShowDropdown(false)}
                     >
-                      Design
+                      Imaging
+                    </Link>
+                    <Link
+                      href="/services/renovation"
+                      className="block px-4 py-2 text-gray-700 hover:bg-[#c62931] hover:text-white rounded-md"
+                      onClick={() => setShowDropdown(false)}
+                    >
+                      Renovations
+                    </Link>
+                    <Link
+                      href="/services/construction"
+                      className="block px-4 py-2 text-gray-700 hover:bg-[#c62931] hover:text-white rounded-md"
+                      onClick={() => setShowDropdown(false)}
+                    >
+                      Construction
+                    </Link>
+                    <Link
+                      href="/services/link"
+                      className="block px-4 py-2 text-gray-700 hover:bg-[#c62931] hover:text-white rounded-md"
+                      onClick={() => setShowDropdown(false)}
+                    >
+                      Link
                     </Link>
                   </div>
                 )}
