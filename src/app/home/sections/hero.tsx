69d2f2ae5f3a87ab3e9b147f8d2ff2485f902a00
Mark Balajadia <markb.freelance@gmail.com>
2025-05-24 06:33:33 +0800

    replaced pump animation on homempage

--- src/app/home/sections/hero.tsx
+++ src/app/home/sections/hero.tsx
@@ -12,15 +12,15 @@ const Hero: React.FC = () => {
         playsInline
         className="absolute top-0 left-0 w-full h-full object-cover z-10"
       >
-        <source src="/videos/Main_v2.mp4" type="video/mp4" />
+        <source src="/videos/PumpAnimation.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       {/* Bottom Blur Overlay */}
-      <div className="absolute -bottom-1 left-0 w-full h-60 z-20 pointer-events-none"
+      {/* <div className="absolute bottom-0 left-0 w-full h-16 z-20 pointer-events-none"
         style={{
           background: 'linear-gradient(to bottom, rgba(255,255,255,0) 0%, rgba(255,255,255,0.6) 50%, rgba(255,255,255,1) 100%)',
         }}
-      />
+      /> */}
       <div className="relative flex flex-col-reverse scrn-600:flex-row scrn-600:ps-2 items-center justify-between bg-transparent
       h-140
       scrn-400:h-140
