60fc9879338e2c3bfdd88eb3143222e996111fd5
Mark Balajadia <markb.freelance@gmail.com>
2025-06-08 03:08:04 +0800

    updated hero files on apec link page

--- src/app/services/link/ClientLayoutWrapper.tsx
+++ src/app/services/link/ClientLayoutWrapper.tsx
@@ -8,8 +8,8 @@ export default function ClientLayoutWrapper({ children }: { children: React.Reac
   const backgroundImages = [
      "/backgrounds/services/link/Background_Link_1.jpg",
      "/backgrounds/services/link/Background_Link_2.jpg",
-    // "/backgrounds/services/link/Background_Link_3.jpg",
-    // "/backgrounds/services/link/Background_Link_4.jpg",
+     "/backgrounds/services/link/Background_Link_3.jpg",
+     "/backgrounds/services/link/Background_Link_4.jpg",
   ];
 
   const backgroundUrl = imageIndex !== null ? backgroundImages[imageIndex] : backgroundImages[0];
