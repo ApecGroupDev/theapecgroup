13cc7329e910cdbc09263052f7537a389541eaf2
Mark Balajadia <markb.freelance@gmail.com>
2025-06-27 14:16:08 +0800

    construction tsx files update

--- src/app/services/construction/layout.tsx
+++ src/app/services/construction/layout.tsx
@@ -1,7 +1,7 @@
 export default function ServicesConstructionLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="bg-[url('/backgrounds/services/construction/Background_Construction.jpg')] bg-cover bg-top bg-no-repeat min-h-screen">
-      <div className="w-full">{children}</div>
+    <div className='bg-[url("/backgrounds/services/construction/Background_Construction.jpg")] bg-cover bg-top bg-no-repeat min-h-screen'>
+      <div className='w-full'>{children}</div>
     </div>
   );
 }
