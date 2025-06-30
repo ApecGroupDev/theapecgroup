91c162ee96de9c986481ecb3c1406d6f7a326474
Mark Balajadia <markb.freelance@gmail.com>
2025-07-01 03:14:47 +0800

    reworked landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,18 @@
-import { redirect } from 'next/navigation';
+import About from "./home/sections/about";
+import FeatureBoxes from "./home/sections/featureboxes";
+import Hero from "./home/sections/hero";
+import MembershipLogosCarousel from "./home/sections/membershiplogoslider";
+import HomePageLayout from "./layouts/homepageLayout";
 
 export default function Page() {
-  redirect('/home'); // Redirect to the Home page
-  return null; // This won't render since the redirect happens immediately
+  return (
+    <HomePageLayout>
+      <div className="flex flex-col">
+        <Hero />
+        <About />
+        <FeatureBoxes />
+        <MembershipLogosCarousel />
+      </div>
+    </HomePageLayout>
+  );
 }
