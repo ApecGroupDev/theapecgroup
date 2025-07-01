70822e95d6ac3c379693778d6aecc2116d8bb195
Mark Balajadia <markb.freelance@gmail.com>
2025-07-01 16:44:52 +0800

    removed financing old directory

--- src/app/services/financing/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function ServicesFinancingLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className='bg-[url("/backgrounds/services/financing/Background_Financing.jpg")] bg-cover bg-top bg-no-repeat min-h-screen'>
-      <div className='w-full'>{children}</div>
-    </div>
-  );
-}
