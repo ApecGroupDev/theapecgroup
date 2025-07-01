74bce526b2c1dde3d5b6863cc3956e9e77358480
Mark Balajadia <markb.freelance@gmail.com>
2025-07-01 16:58:44 +0800

    removed old imaging directory

--- src/app/services/imaging-and-canopies/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function ServicesImagingLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className='bg-[url("/backgrounds/services/imaging-and-canopies/Background_Imaging.jpg")] bg-cover bg-top bg-no-repeat min-h-screen'>
-      <div className='w-full'>{children}</div>
-    </div>
-  );
-}
