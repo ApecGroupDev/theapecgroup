3160a269804312180ada485b71d191617093446d
Mark Balajadia <markb.freelance@gmail.com>
2025-07-01 17:16:23 +0800

    services main hero tsx update

--- src/app/services/sections/hero.tsx
+++ src/app/services/sections/hero.tsx
@@ -42,45 +42,9 @@ const Hero: React.FC = () => {
     <div
       id="hero-section"
       ref={heroRef}
-      className="relative flex pointer-events-none items-center overflow-hidden
-      h-80
-      scrn-450:h-96
-      scrn-500:h-114
-      scrn-600:h-118
-      scrn-700:h-124 
-      scrn-750:h-128
-      scrn-800:h-120
-      scrn-850:h-124 
-      scrn-900:h-128 
-      scrn-950:h-132
-      scrn-1000:h-140
-      scrn-1050:h-144
-      scrn-1100:h-148
-      scrn-1150:h-152
-      scrn-1200:h-160 
-      scrn-1300:h-172
-      scrn-1400:h-180
-      scrn-1450:h-184
-      scrn-1500:h-192
-      scrn-1550:h-198 
-      scrn-1600:h-208
-      scrn-1700:h-228
-      scrn-1800:h-240
-      scrn-1850:h-244
-      scrn-1900:h-264
-      scrn-1950:h-280
-      scrn-2000:h-280
-      scrn-2050:h-288
-      scrn-2100:h-296
-      scrn-2150:h-304
-      scrn-2200:h-312
-      scrn-2250:h-316
-      scrn-2300:h-324
-      scrn-2400:h-339
-      scrn-2500:h-352"
-    >
+      className='relative flex pointer-events-none items-center overflow-hidden h-80 scrn-450:h-96 scrn-500:h-114 scrn-600:h-118 scrn-700:h-124 scrn-750:h-128 scrn-800:h-120 scrn-850:h-124 scrn-900:h-128 scrn-950:h-132 scrn-1000:h-140 scrn-1050:h-144 scrn-1100:h-148 scrn-1150:h-152 scrn-1200:h-160 scrn-1300:h-172 scrn-1400:h-180 scrn-1450:h-184 scrn-1500:h-192 scrn-1550:h-198 scrn-1600:h-208 scrn-1700:h-228 scrn-1800:h-240 scrn-1850:h-244 scrn-1900:h-264 scrn-1950:h-280 scrn-2050:h-288 scrn-2100:h-296 scrn-2150:h-304 scrn-2200:h-312 scrn-2250:h-316 scrn-2300:h-324 scrn-2400:h-339 scrn-2500:h-352'>
       {/* Background Image */}
-      <div className="absolute z-10"
+      <div className='absolute z-10'
       >
         <Image
           src="/backgrounds/services/Hero_Services.png"
@@ -89,78 +53,29 @@ const Hero: React.FC = () => {
           width={2786}
           height={1438}
           priority
-          className="
-          scrn-300:h-64
-          scrn-300:mt-12
-          scrn-400:h-72
-          scrn-400:mt-8
-          scrn-450:h-80
-          scrn-500:h-96
-          scrn-500:mt-10
-          scrn-600:h-114
-          scrn-600:mt-4
-          scrn-700:h-full
-          scrn-700:mt-8
-          scrn-750:pb-10
-          scrn-800:pb-0
-          scrn-800:mt-0
-          scrn-900:mt-0
-          scrn-1900:pb-16
-          scrn-1950:pb-24
-          scrn-2000:pb-20
-          scrn-2150:pb-24
-          scrm-2300:pb-32"
-        />
+          className='scrn-300:h-64 scrn-300:mt-12 scrn-400:h-72 scrn-400:mt-8 scrn-450:h-80 scrn-500:h-96 scrn-500:mt-10 scrn-600:h-114 scrn-600:mt-4 scrn-700:h-full scrn-700:mt-8 scrn-750:pb-10 scrn-800:pb-0 scrn-1900:pb-16 scrn-1950:pb-24 scrn-2000:pb-20 scrn-2150:pb-24 scrn-2300:pb-32'/>
       </div>
 
       {/* Left Text Section */}
-      <div className={`fixed
-        scrn-800:pb-20
-        scrn-1000:pb-12
-        scrn-1200:pb-0
-        scrn-750:ms-0 
-        p-2 
-        scrn-600:ps-4
-        scrn-1250:ps-12 
-        text-left transition-opacity duration-75
+      <div className={`fixed scrn-800:pb-20 scrn-1000:pb-12 scrn-1200:pb-0 scrn-750:ms-0 p-2 scrn-600:ps-4 scrn-1250:ps-12 text-left transition-opacity duration-75
         ${fadeOut ? 'opacity-0 pointer-events-none' : 'opacity-100'}`}
       >
         {/* Breadcrumb */}
-        <nav className="">
-          <ul className="flex scrn-600:items-center space-x-2 font-semibold text-gray-600 tracking-widest
-            text-2xs 
-            scrn-600:text-xs
-            scrn-750:text-sm
-            scrn-1000:text-lg 
-            scrn-1500:text-xl"
-          >
+        <nav>
+          <ul className='flex scrn-600:items-center space-x-2 font-semibold text-gray-600 tracking-widest text-2xs scrn-600:text-xs scrn-750:text-sm scrn-1000:text-lg scrn-1500:text-xl'>
             <li>
-              <Link href="/home" className="hover:text-[#c62931]">
+              <Link href="/home" className='hover:text-[#c62931]'>
                 Home
               </Link>
             </li>
             <li>/</li>
-            <li className="text-[#c62931]">Services</li>
+            <li className='text-[#c62931]'>Services</li>
           </ul>
         </nav>
 
         {/* Headline */}
         <div>
-          <span className="scale-y-110 text-[#c62931] p-0 m-0 font-semibold whitespace-nowrap
-            text-5xl 
-            scrn-400:text-7xl
-            scrn-600:text-8xl 
-            scrn-750:text-9xl 
-            scrn-800:text-9xl 
-            scrn-900:text-9xl 
-            scrn-1000:text-10xl 
-            scrn-1250:text-11xl 
-            scrn-1500:text-12xl 
-            scrn-1700:text-13xl 
-            scrn-1900:text-14xl
-            scrn-2200:text-16xl 
-            scrn-2400:text-18xl"
-          >
+          <span className='scale-y-110 text-[#c62931] p-0 m-0 font-semibold whitespace-nowrap text-5xl scrn-400:text-7xl scrn-600:text-8xl scrn-750:text-9xl scrn-800:text-9xl scrn-900:text-9xl scrn-1000:text-10xl scrn-1250:text-11xl scrn-1500:text-12xl scrn-1700:text-13xl scrn-1900:text-14xl scrn-2200:text-16xl scrn-2400:text-18xl'>
             SERVICES
           </span>
         </div>
