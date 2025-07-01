a577b0af33a2a27015d6fe89b51d84a3b3c8fe98
Mark Balajadia <markb.freelance@gmail.com>
2025-07-01 18:02:17 +0800

    services layout tsx update

--- src/app/services/layout.tsx
+++ src/app/services/layout.tsx
@@ -2,9 +2,9 @@ import Header from "@/components/header";
 
 export default function ServicesLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="bg-[url('/backgrounds/services/Background_Services.jpg')] bg-cover bg-top bg-no-repeat min-h-screen">
+    <div className='bg-[url("/backgrounds/services/Background_Services.jpg")] bg-cover bg-top bg-no-repeat min-h-screen'>
       <Header />
-      <div className="w-full">{children}</div>
+      <div className='w-full'>{children}</div>
     </div>
   );
 }
