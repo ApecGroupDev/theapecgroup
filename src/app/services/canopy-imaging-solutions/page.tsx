61bae2f4c64ef8fd987f931ed26ddef200bcf42c
Mark Balajadia <markb.freelance@gmail.com>
2025-07-02 20:12:16 +0800

    canopy imaging solution metadata

--- src/app/services/canopy-imaging-solutions/page.tsx
+++ src/app/services/canopy-imaging-solutions/page.tsx
@@ -2,12 +2,17 @@ import Footer from "@/components/footer";
 import MainImaging from "./sections/main";
 import Hero from "./sections/hero";
 
+export const metadata = {
+  title: "Canopy Imaging Solutions | APEC",
+  description: "Transform gas stations with APEC's advanced canopy imaging services. Visit theapecgroup.com/services/imaging-and-canopies for cutting-edge visual designs.",
+};
+
 const ServicesImaging: React.FC = () => {
   return (
     <div className='flex flex-col'>
-        <Hero />
-        <MainImaging />
-        <Footer />
+      <Hero />
+      <MainImaging />
+      <Footer />
     </div>
   );
 };
