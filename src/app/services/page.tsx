3b584fb9bac5fa200fac4d25a03ee6e8ed8b2a87
Mark Balajadia <markb.freelance@gmail.com>
2025-07-02 20:06:19 +0800

    services landing page metadata

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -2,6 +2,11 @@ import Hero from "./sections/hero";
 import MainServices from "./sections/main";
 import Footer from "@/components/footer";
 
+export const metadata = {
+  title: "Petroleum Equipment Services | The APEC Group",
+  description: "Achieve regulatory compliance with APEC's specialized environmental services for petroleum. Visit theapecgroup.com/services/compliance for sustainable solutions.",
+};
+
 const Services: React.FC = () => {
   return (
     <div className='flex flex-col'>
