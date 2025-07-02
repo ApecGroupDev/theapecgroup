2015532dd72553e24fa2d69f034c619e3d4f0980
Mark Balajadia <markb.freelance@gmail.com>
2025-07-02 20:31:12 +0800

    environmental-compliance-solutions metadata

--- src/app/services/environmental-compliance-solutions/page.tsx
+++ src/app/services/environmental-compliance-solutions/page.tsx
@@ -1,6 +1,12 @@
 import Footer from "@/components/footer";
 import MainFinancing from "./sections/main";
 import Hero from "./sections/hero";
+
+export const metadata = {
+  title: "Environmental Compliance Solutions | APEC",
+  description: "Stay compliant with APEC's tailored environmental services for petroleum industries. Visit theapecgroup.com/services/compliance for eco-friendly solutions.",
+};
+
 const ServicesCompliance: React.FC = () => {
   return (
     <div className='flex flex-col'>
