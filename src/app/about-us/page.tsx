a63e06a7120c8b43ada728232f1b854b89c21cf4
Mark Balajadia <markb.freelance@gmail.com>
2025-07-02 18:48:10 +0800

    about us page metadata

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -3,6 +3,11 @@ import ChooseUs from "./sections/chooseus";
 import Hero from "./sections/hero";
 import HorizontalScrollSection from "./sections/principles";
 
+export const metadata = {
+  title: 'About The APEC Group | Petroleum Services',
+  description: 'Discover The APEC Group’s legacy in petroleum services since 1989. Explore our innovative energy solutions and commitment to excellence.',
+};
+
 const About: React.FC = () => {
   return (
     <div className='flex flex-col'>
