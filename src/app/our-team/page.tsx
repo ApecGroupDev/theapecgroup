fc6a9d474b3c6e815c0ab0ba311fb78acec41421
Mark Balajadia <markb.freelance@gmail.com>
2025-07-02 19:26:53 +0800

    team page metadata

--- src/app/our-team/page.tsx
+++ src/app/our-team/page.tsx
@@ -1,6 +1,11 @@
 import Hero from "./sections/hero";
 import TeamPage from "./sections/main";
 
+export const metadata = {
+  title: "Meet Our Team | The APEC Group",
+  description: "Meet The APEC Group’s expert team, delivering innovative petroleum solutions since 1989. Learn about our dedicated professionals.",
+};
+
 const About: React.FC = () => {
   return (
     <div className='flex flex-col'>
