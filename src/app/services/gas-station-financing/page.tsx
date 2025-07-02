09b593c7ab4c06eaa1205f74550cf6ceb03ef6d1
Mark Balajadia <markb.freelance@gmail.com>
2025-07-02 20:41:52 +0800

    gas-station-financing metadata

--- src/app/services/gas-station-financing/page.tsx
+++ src/app/services/gas-station-financing/page.tsx
@@ -1,6 +1,12 @@
 import Footer from "@/components/footer";
 import MainFinancing from "./sections/main";
 import Hero from "./sections/hero";
+
+export const metadata = {
+  title: "Gas Station Financing Solutions | APEC",
+  description: "Secure funding for gas stations with APEC's tailored financing services. Visit theapecgroup.com/services/financing for flexible energy project solutions.",
+};
+
 const ServicesFinancing: React.FC = () => {
   return (
     <div className='flex flex-col'>
