9f98b32c145f05fd107a0febfe92d72771a06297
Mark Balajadia <markb.freelance@gmail.com>
2025-07-08 03:39:18 +0800

    hero section update, added one stop shop logo gif

--- src/app/home/sections/hero.tsx
+++ src/app/home/sections/hero.tsx
@@ -22,8 +22,13 @@ const Hero: React.FC = () => {
       /> */}
       <div className='relative flex flex-col-reverse scrn-600:flex-row scrn-600:ps-2 items-center justify-between bg-transparent h-140 scrn-400:h-140 scrn-600:h-124 scrn-750:h-120 scrn-800:h-124 scrn-900:h-136 scrn-1000:h-160 scrn-1100:h-168 scrn-1150:h-168 scrn-1200:h-176 scrn-1300:h-188 scrn-1400:h-198 scrn-1500:h-216 scrn-1600:h-228 scrn-1700:h-248 scrn-1800:h-260 scrn-1900:h-272 scrn-2000:h-280 scrn-2100:h-288 scrn-2200:h-304 scrn-2300:h-308 scrn-2350:h-320 scrn-2400:h-324 scrn-2500:h-339'>
         {/* Left Text Section */}
-        <div className='scrn-1500:ms-4 scrn-2300:ms-8 -space-y-1.5 text-center mb-6 scrn-400:mb-0 scrn-600:text-left scrn-1000:pb-5 z-20 hidden scrn-600:block scrn-600:pt-20 scrn-750:pt-12 scrn-1000:pt-20 scrn-1300:pt-24 scrn-1800:pt-32'>
-          <p className='text-xl scrn-700:text-2xl scrn-900:text-2xl scrn-1000:text-2xl scrn-1200:text-3xl scrn-1500:text-4xl scrn-1700:text-4xl scrn-1800:text-5xl scrn-2300:text-6xl font-bold whitespace-nowrap'>
+        <div className='scrn-1500:ms-16 scrn-2300:ms-8 -space-y-1.5 text-center mb-6 scrn-400:mb-0 scrn-600:text-left scrn-1000:pb-5 z-20 hidden scrn-600:block scrn-600:pt-20 scrn-750:pt-12 scrn-1000:pt-20 scrn-1300:pt-24 scrn-1800:pt-32'>
+          <img
+            src="/logos/One-Stop-Shop.gif"
+            alt="Animated Logo"
+            className="mx-auto scrn-600:mx-0 w-40 scrn-800:w-56 scrn-1000:w-64 scrn-1200:w-72 scrn-1500:w-80 scrn-1800:w-140"
+          />
+          {/* <p className='text-xl scrn-700:text-2xl scrn-900:text-2xl scrn-1000:text-2xl scrn-1200:text-3xl scrn-1500:text-4xl scrn-1700:text-4xl scrn-1800:text-5xl scrn-2300:text-6xl font-bold whitespace-nowrap'>
             YOUR TOP CHOICE
           </p>
           <p className='text-2xl scrn-700:text-3xl scrn-900:text-4xl scrn-1000:text-4xl scrn-1200:text-5xl scrn-1500:text-6xl scrn-1700:text-6xl scrn-1800:text-7xl scrn-2300:text-8xl font-regular italic text-[#c62931] tracking-wider whitespace-nowrap'>
@@ -31,7 +36,7 @@ const Hero: React.FC = () => {
           </p>
           <p className='text-xl scrn-700:text-2xl scrn-900:text-2xl scrn-1000:text-2xl scrn-1200:text-3xl scrn-1500:text-4xl scrn-1700:text-4xl scrn-1800:text-5xl scrn-2300:text-6xl font-regular'>
             ATLANTA COMPANY
-          </p>
+          </p> */}
         </div>
 
         {/* Right Image Section - Coded Out for video animation*/}
