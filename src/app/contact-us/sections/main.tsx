a3ea3dd928ab4485919255402bb0648a2098938b
Mark Balajadia <markb.freelance@gmail.com>
2025-07-11 03:34:12 +0800

    added base map to contacts

--- src/app/contact-us/sections/main.tsx
+++ src/app/contact-us/sections/main.tsx
@@ -67,12 +67,24 @@ const MainContact: React.FC = () => {
             <ContactPageForm />
           </div>
         </div>
-        <p className='space-x-2 font-semibold text-nowrap hidden text-transparent pt-6 scrn-400:pt-6 scrn-450:pt-12 scrn-500:pt-8 scrn-550:pt-8 scrn-600:pt-24 scrn-700:pt-20 scrn-750:pt-16 scrn-800:pt-8 scrn-850:pt-8 scrn-900:pt-12 scrn-950:pt-16 scrn-1000:pt-28 scrn-1200:pt-28 scrn-1300:pt-40 scrn-1350:pt-48 scrn-1400:pt-56 scrn-1450:pt-64 scrn-1500:pt-40 scrn-1600:pt-48 scrn-1800:pt-64 scrn-1900:pt-72 scrn-2400:pt-96'>
-          <span className='scrn-300:text-2xl scrn-450:text-3xl scrn-600:text-4xl scrn-700:text-5xl scrn-750:text-4xl scrn-800:text-4xl scrn-850:text-5xl scrn-900:text-5xl scrn-1000:text-6xl scrn-1250:text-7xl scrn-1500:text-8xl scrn-1700:text-8xl scrn-1900:text-8xl scrn-2000:text-9xl'>
-          </span>
-          <span className='scrn-300:text-base scrn-450:text-xl scrn-600:text-2xl scrn-750:text-3xl scrn-800:text-3xl scrn-900:text-3xl scrn-1000:text-4xl scrn-1200:text-5xl scrn-1500:text-5xl scrn-1700:text-6xl scrn-1900:text-6xl scrn-2000:text-7xl'>
-          </span>
-        </p>
+      </div>
+      {/* Google Maps Section */}
+      <div className="w-full mt-12 px-4 scrn-600:px-10 scrn-800:px-20">
+        <h2 className="text-center text-xl font-semibold text-gray-800 mb-4 scrn-600:text-2xl scrn-1000:text-3xl">
+          Find Us on Google Maps
+        </h2>
+        <div className="w-4/5 mx-auto h-[300px] scrn-600:h-[400px] scrn-1000:h-[500px] rounded-xl overflow-hidden shadow-2xl">
+          <iframe
+            title="Google Map Location"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5625.444795644097!2d-84.19747312274932!3d33.86014347322978!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f5a60d37eeaeb9%3A0x378e67f08f914808!2sAtlanta%20Petroleum%20Equipment%20Co!5e1!3m2!1sen!2sph!4v1752174129484!5m2!1sen!2sph"
+            width="100%"
+            height="100%"
+            style={{ border: 0 }}
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
       </div>
     </div>
   );
