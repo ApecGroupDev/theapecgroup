3358748e64ba020ecde4123996560f0eff273931
Mark Balajadia <markb.freelance@gmail.com>
2025-07-11 11:26:12 +0800

    home/hero img to Image

--- src/app/home/sections/hero.tsx
+++ src/app/home/sections/hero.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import Image from 'next/image';
+import Image from 'next/image';
 
 const Hero: React.FC = () => {
   return (
@@ -23,10 +23,12 @@ const Hero: React.FC = () => {
       <div className='relative flex flex-col-reverse scrn-600:flex-row scrn-600:ps-2 items-center justify-between bg-transparent h-140 scrn-400:h-140 scrn-600:h-124 scrn-750:h-120 scrn-800:h-124 scrn-900:h-136 scrn-1000:h-160 scrn-1100:h-168 scrn-1150:h-168 scrn-1200:h-176 scrn-1300:h-188 scrn-1400:h-198 scrn-1500:h-216 scrn-1600:h-228 scrn-1700:h-248 scrn-1800:h-260 scrn-1900:h-272 scrn-2000:h-280 scrn-2100:h-288 scrn-2200:h-304 scrn-2300:h-308 scrn-2350:h-320 scrn-2400:h-324 scrn-2500:h-339'>
         {/* Left Text Section */}
         <div className='scrn-1500:ms-16 scrn-2300:ms-8 -space-y-1.5 text-center mb-6 scrn-400:mb-0 scrn-600:text-left scrn-1000:pb-5 z-20 hidden scrn-600:block scrn-600:pt-20 scrn-750:pt-12 scrn-1000:pt-20 scrn-1300:pt-24 scrn-1800:pt-32'>
-          <img
+          <Image
             src="/logos/One-Stop-Shop.gif"
             alt="Animated Logo"
-            className="mx-auto scrn-600:mx-0
+            width={800} // Provide an appropriate width
+            height={800} // Provide an appropriate height
+            className='mx-auto scrn-600:mx-0
             w-48 
             scrn-700:w-60
             scrn-800:w-64
@@ -35,7 +37,7 @@ const Hero: React.FC = () => {
             scrn-1100:w-96 
             scrn-1500:w-120
             scrn-1600:w-128
-            scrn-1800:w-140"
+            scrn-1800:w-140'
           />
           {/* <p className='text-xl scrn-700:text-2xl scrn-900:text-2xl scrn-1000:text-2xl scrn-1200:text-3xl scrn-1500:text-4xl scrn-1700:text-4xl scrn-1800:text-5xl scrn-2300:text-6xl font-bold whitespace-nowrap'>
             YOUR TOP CHOICE
