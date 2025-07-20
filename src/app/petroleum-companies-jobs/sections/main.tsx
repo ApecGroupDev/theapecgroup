fefbe8f1af1bf7183b81f4c8d485d1a39f6b8f6b
Mark Balajadia <markb.freelance@gmail.com>
2025-07-20 22:42:45 +0800

    careers additional base content update

--- src/app/petroleum-companies-jobs/sections/main.tsx
+++ src/app/petroleum-companies-jobs/sections/main.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import Image from 'next/image';
+import CareersForm from '@/components/careersForm';
 
 const CareersMain: React.FC = () => {
   return (
@@ -9,7 +10,6 @@ const CareersMain: React.FC = () => {
 
       {/* First Row */}
       <div className='flex w-full max-w-[1200px] gap-4'>
-
         {/* 2/3 Box - Job Openings with Background Image */}
         <div className='w-2/3 relative rounded-xl overflow-hidden shadow-md'>
           {/* Background Image */}
@@ -19,10 +19,8 @@ const CareersMain: React.FC = () => {
             fill
             className="object-cover"
           />
-
           {/* Overlay for readability */}
           <div className="absolute inset-0 bg-black/60"></div>
-
           {/* Content */}
           <div className='relative p-6 flex text-xl flex-col justify-center h-full text-white'>
             <h2 className='font-semibold mb-4'>Current Openings</h2>
@@ -33,35 +31,27 @@ const CareersMain: React.FC = () => {
             </ul>
           </div>
         </div>
-
         {/* 1/3 Box - Main Office Address */}
         <div className='w-1/3 bg-gray-500 rounded-xl text-white shadow-md p-6 flex flex-col justify-center'>
           <h2 className='text-xl font-semibold mb-4'>Main Office</h2>
-          <p className=''>
-            Atlanta, Georgia<br />
-            Location Place<br />
+          <p className='font-semibold'>
             4732-E North Royal Atlanta Drive<br />
             Tucker, Georgia 30084
           </p>
         </div>
-
       </div>
 
 
-      {/* First Row */}
+      {/* Second Row */}
       <div className='flex w-full max-w-[1200px] gap-4'>
-
         {/* 1/3 Box - Main Office Address */}
         <div className='w-1/3 bg-red-600 rounded-xl text-white shadow-md p-6 flex flex-col justify-center'>
           <h2 className='text-xl font-semibold mb-4'>Branch Office</h2>
-          <p className=''>
-            Houston, Texas<br />
-            Location Place<br />
+          <p className='font-semibold'>
             505 Garden Oaks Boulevard<br />
             Houston, Texas 77018
           </p>
         </div>
-
         {/* 2/3 Box - Job Openings with Background Image */}
         <div className='w-2/3 relative rounded-xl overflow-hidden shadow-md'>
           {/* Background Image */}
@@ -71,10 +61,8 @@ const CareersMain: React.FC = () => {
             fill
             className="object-cover"
           />
-
           {/* Overlay for readability */}
           <div className="absolute inset-0 bg-black/60"></div>
-
           {/* Content */}
           <div className='relative p-6 flex text-xl flex-col justify-center h-full text-white'>
             <h2 className='font-semibold mb-4'>Current Openings</h2>
@@ -84,6 +72,17 @@ const CareersMain: React.FC = () => {
               <li>Construction Assistant</li>
             </ul>
           </div>
+        </div>
+      </div>
+
+      <div className='relative rounded-3xl shadow-2xl mt-6 scrn-400:mt-6 scrn-450:mt-6 scrn-500:mt-8 scrn-550:mt-8 scrn-750:mt-0
+      scrn-1900:max-w-6xl'>
+        <div className='absolute inset-0 bg-white/30 backdrop-blur-md rounded-3xl z-0'></div>
+        <div className='relative z-10 p-4 scrn-800:p-8 scrn-900:p-10 scrn-1000:p-12 scrn-1250:p-16 scrn-1500:p-24'>
+          <span className='font-medium scrn-300:text-base scrn-500:text-xl scrn-600:text-xl scrn-750:text-lg scrn-800:text-lg scrn-900:text-xl scrn-1000:text-xl scrn-1250:text-2xl scrn-1500:text-3xl scrn-1700:text-3xl scrn-1900:text-4xl scrn-2000:text-4xl'>
+            SEND YOUR INFORMATION HERE
+          </span>
+          <CareersForm />
         </div>
       </div>
 
