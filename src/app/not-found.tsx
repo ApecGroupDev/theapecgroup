dc661be564b42385554c9d03e7a384b41abe43e0
Mark Balajadia <markb.freelance@gmail.com>
2025-07-26 03:28:22 +0800

    last push error fix &apos;

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,7 +7,7 @@ export default function NotFound() {
     <div className="flex flex-col items-center justify-center h-screen text-center">
       <h1 className="text-5xl scrn-1500:text-7xl font-bold text-red-600">404</h1>
       <p className="text-xl scrn-1500:text-3xl mt-4 text-gray-700">Page Not Found</p>
-      <p className="mt-2 scrn-1500:text-2xl text-gray-500">The page you're looking for does not exist.</p>
+      <p className="mt-2 scrn-1500:text-2xl text-gray-500">The page you&apos;re looking for does not exist.</p>
 
       <Link
         href="/"
