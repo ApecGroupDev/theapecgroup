766124be7fdbb5d4dae0714b040cebcea3ff6abe
Mark Balajadia <markb.freelance@gmail.com>
2025-07-27 08:01:54 +0800

    About Us image update, all screen

--- src/app/about-us/sections/hero.tsx
+++ src/app/about-us/sections/hero.tsx
@@ -43,9 +43,9 @@ const Hero: React.FC = () => {
       ref={heroRef}
       className='relative flex items-center h-116 scrn-350:h-120 scrn-400:h-132 scrn-450:h-140 scrn-500:h-148 scrn-550:h-160 scrn-600:h-114 scrn-700:h-124 scrn-750:h-128 scrn-800:h-120 scrn-850:h-124 scrn-900:h-128 scrn-950:h-132 scrn-1000:h-140 scrn-1050:h-144 scrn-1100:h-148 scrn-1150:h-152 scrn-1200:h-160 scrn-1300:h-172 scrn-1400:h-180 scrn-1450:h-184 scrn-1500:h-192 scrn-1550:h-198 scrn-1600:h-208 scrn-1700:h-228 scrn-1800:h-240 scrn-1850:h-244 scrn-1900:h-264 scrn-1950:h-272 scrn-2000:h-280 scrn-2050:h-288 scrn-2100:h-296 scrn-2150:h-304 scrn-2200:h-312 scrn-2250:h-316 scrn-2300:h-324 scrn-2400:h-339 scrn-2500:h-352'>
 
-      <div className='absolute inset-0 z-10 hidden scrn-600:block scrn-600:mt-28 scrn-650:mt-24 scrn-800:mt-4 scrn-1100:mt-0 scrn-1900:scale-y-110 scrn-1900:mt-12'>
+      <div className='absolute inset-0 z-10 hidden scrn-500:block mt-64 scrn-550:mt-60 scrn-600:mt-44 scrn-650:mt-36 scrn-750:mt-28 scrn-800:mt-24 scrn-1000:mt-24 scrn-1100:mt-24 scrn-1150:mt-16 scrn-1200:mt-24 scrn-1700:mt-28 scrn-1900:scale-y-110 scrn-1900:mt-16'>
         <Image
-          src="/backgrounds/aboutUs/Hero_About.png"
+          src="/backgrounds/aboutUs/Hero_About_Flipped.png"
           alt="Background"
           style={{ objectFit: 'cover' }}
           width={2786}
@@ -54,9 +54,9 @@ const Hero: React.FC = () => {
         />
       </div>
 
-      <div className='absolute inset-0 z-10 scrn-600:hidden mt-12 scrn-350:mt-0'>
+      <div className='absolute inset-0 z-10 scrn-500:hidden mt-16 scrn-350:mt-12 scrn-400:mt-4 scrn-450:-mt-8 scrn-500:-mt-12'>
         <Image
-          src="/backgrounds/aboutUs/Hero_About_Mobile.png"
+          src="/backgrounds/aboutUs/Hero_About_Mobile_Test.png"
           alt="Background"
           style={{ objectFit: 'cover' }}
           width={2786}
