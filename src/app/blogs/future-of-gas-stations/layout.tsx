879c71b4bff7b00616f3dcd7c13827a6ddc4c309
Mark Balajadia <markb.freelance@gmail.com>
2025-08-03 20:33:13 +0800

    last push error fix

--- src/app/blogs/future-of-gas-stations/layout.tsx
+++ src/app/blogs/future-of-gas-stations/layout.tsx
@@ -1,5 +1,3 @@
-import Header from '@/components/header';
-
 export default function BlogLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className='bg-white bg-cover bg-top bg-no-repeat min-h-screen'>
