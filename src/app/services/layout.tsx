c2264b2273e3131242ec9aa783d784149f096bd6
Mark Balajadia <markb.freelance@gmail.com>
2025-08-03 12:21:25 +0800

    services hero bg and text update

--- src/app/services/layout.tsx
+++ src/app/services/layout.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/header";
 
 export default function ServicesLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='bg-[url("/backgrounds/services/Background_Services.jpg")] bg-cover bg-top bg-no-repeat min-h-screen'>
+    <div className='bg-[url("/backgrounds/services/Background_Services_Intact.jpg")] bg-cover bg-top bg-no-repeat min-h-screen'>
       <Header />
       <div className='w-full'>{children}</div>
     </div>
