57fb894d68226ba5d00e6bcce2e5c299b344c9ba
Mark Balajadia <markb.freelance@gmail.com>
2025-08-03 11:29:10 +0800

    contact us page hero text and background fix

--- src/app/contact-us/layout.tsx
+++ src/app/contact-us/layout.tsx
@@ -2,7 +2,7 @@ import Header from '@/components/header';
 
 export default function ContactLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='bg-[url("/backgrounds/contact/Background_Contact.jpg")] bg-cover bg-top bg-no-repeat min-h-screen'>
+    <div className='bg-[url("/backgrounds/contact/Background_Contact_Intact.jpg")] bg-cover bg-top bg-no-repeat min-h-screen'>
       <Header />
       <div className='w-full'>{children}</div>
     </div>
