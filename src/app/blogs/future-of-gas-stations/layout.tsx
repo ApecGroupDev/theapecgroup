27e82f1448ebb348db93bdf262fdb51d5a2de4a7
Mark Balajadia <markb.freelance@gmail.com>
2025-08-03 20:25:27 +0800

    future of gas stations separate blog

---
+++ src/app/blogs/future-of-gas-stations/layout.tsx
@@ -0,0 +1,9 @@
+import Header from '@/components/header';
+
+export default function BlogLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className='bg-white bg-cover bg-top bg-no-repeat min-h-screen'>
+      <div className='w-full'>{children}</div>
+    </div>
+  );
+}
--- src/app/blogs/future-of-gas-stations/page.tsx
+++ src/app/blogs/future-of-gas-stations/page.tsx
@@ -1,6 +1,5 @@
 import Footer from "@/components/footer";
-import BlogMain from "./sections/main";
-import Hero from "./sections/hero";
+import Main from "./sections/main";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -14,9 +13,13 @@ export const metadata: Metadata = {
 const Contact: React.FC = () => {
   return (
     <div className='flex flex-col'>
-      <Hero />
-      <BlogMain />
-      <Footer />
+      <Main />
+      <div className="w-full">
+        <div className="h-4 bg-gradient-to-r from-gray-800 to-red-600 -mb-1" />
+      </div>
+      <div className='bg-gray-800'>
+        <Footer />
+      </div>
     </div>
   );
 };
