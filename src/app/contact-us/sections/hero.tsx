e835611281dcd1d62cf1db719109bbf65e2b9d80
Mark Balajadia <markb.freelance@gmail.com>
2025-08-03 11:33:06 +0800

    last push error fix

--- src/app/contact-us/sections/hero.tsx
+++ src/app/contact-us/sections/hero.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState, useRef } from 'react';
-import Image from 'next/image';
+// import Image from 'next/image';
 import Link from 'next/link';
 
 const Hero: React.FC = () => {
