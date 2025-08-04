8b7c814d87ccc4aec777512e8d8d2abc61f5d06c
Mark Balajadia <markb.freelance@gmail.com>
2025-08-05 00:26:36 +0800

    services financing hero update

--- src/app/services/gas-station-financing/layout.tsx
+++ src/app/services/gas-station-financing/layout.tsx
@@ -1,6 +1,6 @@
 export default function ServicesFinancingLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='bg-[url("/backgrounds/services/financing/Background_Financing.jpg")] bg-cover bg-top bg-no-repeat min-h-screen'>
+    <div className='bg-[url("/backgrounds/services/financing/Background_Financing_Intact.jpg")] bg-cover bg-top bg-no-repeat min-h-screen'>
       <div className='w-full'>{children}</div>
     </div>
   );
