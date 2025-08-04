a7eb91aa88b15b43a6d1749a3db576891bd8891c
Mark Balajadia <markb.freelance@gmail.com>
2025-08-05 00:23:50 +0800

    services imaging hero update

--- src/app/services/canopy-imaging-solutions/layout.tsx
+++ src/app/services/canopy-imaging-solutions/layout.tsx
@@ -1,6 +1,6 @@
 export default function ServicesImagingLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='bg-[url("/backgrounds/services/imaging-and-canopies/Background_Imaging.jpg")] bg-cover bg-top bg-no-repeat min-h-screen'>
+    <div className='bg-[url("/backgrounds/services/imaging-and-canopies/Background_Imaging_Intact.jpg")] bg-cover bg-top bg-no-repeat min-h-screen'>
       <div className='w-full'>{children}</div>
     </div>
   );
