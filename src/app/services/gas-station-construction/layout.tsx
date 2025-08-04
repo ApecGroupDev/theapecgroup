936d8f6ce27dd4442704de26a509c523cec0f1c4
Mark Balajadia <markb.freelance@gmail.com>
2025-08-05 00:17:55 +0800

    services construction hero update

--- src/app/services/gas-station-construction/layout.tsx
+++ src/app/services/gas-station-construction/layout.tsx
@@ -1,6 +1,6 @@
 export default function ServicesConstructionLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='bg-[url("/backgrounds/services/construction/Background_Construction.jpg")] bg-cover bg-top bg-no-repeat min-h-screen'>
+    <div className='bg-[url("/backgrounds/services/construction/Background_Construction_Intact.jpg")] bg-cover bg-top bg-no-repeat min-h-screen'>
       <div className='w-full'>{children}</div>
     </div>
   );
