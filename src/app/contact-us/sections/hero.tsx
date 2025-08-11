e0698189452de2e563d243771b8b5f136055accd
Mark Balajadia <markb.freelance@gmail.com>
2025-08-12 02:49:53 +0800

    contact-us nav issue /home to / fix

--- src/app/contact-us/sections/hero.tsx
+++ src/app/contact-us/sections/hero.tsx
@@ -42,7 +42,7 @@ const Hero: React.FC = () => {
     <div
       id="hero-section"
       ref={heroRef}
-      className='relative flex items-center pointer-events-none overflow-hidden h-116 scrn-400:h-116 scrn-450:h-118 scrn-500:h-118 scrn-550:h-118 scrn-600:h-120 scrn-650:h-120 scrn-700:h-120 scrn-800:h-120 scrn-850:h-124 scrn-900:h-128 scrn-950:h-132 scrn-1000:h-144 scrn-1100:h-148 scrn-1150:h-156 scrn-1200:h-164 scrn-1250:h-168 scrn-1300:h-176 scrn-1350:h-180 scrn-1400:h-188 scrn-1450:h-195 scrn-1500:h-198 scrn-1550:h-208 scrn-1600:h-216 scrn-1650:h-224 scrn-1700:h-228 scrn-1750:h-232 scrn-1800:h-240 scrn-1850:h-248 scrn-1900:h-256 scrn-1950:h-260 scrn-2000:h-268 scrn-2050:h-276 scrn-2100:h-280 scrn-2150:h-288 scrn-2200:h-296 scrn-2250:h-304 scrn-2300:h-308 scrn-2350:h-312 scrn-2400:h-316 scrn-2450:h-324 scrn-2500:h-326'>
+      className='relative flex items-center overflow-hidden h-116 scrn-400:h-116 scrn-450:h-118 scrn-500:h-118 scrn-550:h-118 scrn-600:h-120 scrn-650:h-120 scrn-700:h-120 scrn-800:h-120 scrn-850:h-124 scrn-900:h-128 scrn-950:h-132 scrn-1000:h-144 scrn-1100:h-148 scrn-1150:h-156 scrn-1200:h-164 scrn-1250:h-168 scrn-1300:h-176 scrn-1350:h-180 scrn-1400:h-188 scrn-1450:h-195 scrn-1500:h-198 scrn-1550:h-208 scrn-1600:h-216 scrn-1650:h-224 scrn-1700:h-228 scrn-1750:h-232 scrn-1800:h-240 scrn-1850:h-248 scrn-1900:h-256 scrn-1950:h-260 scrn-2000:h-268 scrn-2050:h-276 scrn-2100:h-280 scrn-2150:h-288 scrn-2200:h-296 scrn-2250:h-304 scrn-2300:h-308 scrn-2350:h-312 scrn-2400:h-316 scrn-2450:h-324 scrn-2500:h-326'>
 
       {/* Left Text Section */}
       <div className={`fixed scrn-750:ms-0 p-2 scrn-600:ps-4 text-left transition-opacity duration-75
@@ -52,7 +52,7 @@ const Hero: React.FC = () => {
         <nav>
           <ul className='flex scrn-600:items-center space-x-2 font-semibold text-gray-600 tracking-widest text-2xs scrn-600:text-xs scrn-750:text-sm scrn-1000:text-lg scrn-1500:text-xl'>
             <li>
-              <Link href="/home">
+              <Link href="/" className='hover:text-[#c62931]'>
                 Home
               </Link>
             </li>
