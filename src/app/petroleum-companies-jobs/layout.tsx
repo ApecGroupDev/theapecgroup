c3581861fcfc964f4668c2a0413a02a7002b1942
Mark Balajadia <markb.freelance@gmail.com>
2025-08-16 17:51:49 +0800

    careers page new webp background

--- src/app/petroleum-companies-jobs/layout.tsx
+++ src/app/petroleum-companies-jobs/layout.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/header";
 
 export default function CareersLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='bg-[url("/backgrounds/careers/Background_Careers.png")] bg-cover bg-top bg-no-repeat min-h-screen'>
+    <div className='bg-[url("/backgrounds/careers/Background_Careers.webp")] bg-cover bg-top bg-no-repeat min-h-screen'>
       <Header />
       <div className='w-full'>{children}</div>
     </div>
