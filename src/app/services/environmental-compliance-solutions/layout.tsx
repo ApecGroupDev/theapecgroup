62069b5a731b0e47ceff2ce75c7a69c4ecf822e9
Mark Balajadia <markb.freelance@gmail.com>
2025-08-17 02:36:59 +0800

    reworked background webp codes for services subpages

--- src/app/services/environmental-compliance-solutions/layout.tsx
+++ src/app/services/environmental-compliance-solutions/layout.tsx
@@ -1,6 +1,6 @@
 export default function ServicesComplianceLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='bg-[url("/backgrounds/services/compliance/Background_Compliance_Intact.jpg")] bg-cover bg-top bg-no-repeat min-h-screen'>
+    <div className='bg-[url("/backgrounds/services/compliance/Background_Compliance.webp")] bg-cover bg-top bg-no-repeat min-h-screen'>
       <div className='w-full'>{children}</div>
     </div>
   );
--- src/app/services/gas-station-electrical/layout.tsx
+++ src/app/services/gas-station-electrical/layout.tsx
@@ -1,6 +1,6 @@
 export default function ServicesConstructionLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='bg-[url("/backgrounds/services/electrical/Background_Electrical_Intact.jpg")] bg-cover bg-top bg-no-repeat min-h-screen'>
+    <div className='bg-[url("/backgrounds/services/electrical/Background_Electrical.webp")] bg-cover bg-top bg-no-repeat min-h-screen'>
       <div className='w-full'>{children}</div>
     </div>
   );
