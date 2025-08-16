78d4c98002269798d71b1537c7a3221d7281e266
Mark Balajadia <markb.freelance@gmail.com>
2025-08-17 01:20:09 +0800

    corrected webp background codes

--- src/app/our-team/layout.tsx
+++ src/app/our-team/layout.tsx
@@ -3,7 +3,7 @@ import Footer from "@/components/footer";
 
 export default function TeamLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='bg-[url("/backgrounds/ourTeam/Background_Team_Intact_2.png")] bg-cover bg-top bg-no-repeat min-h-screen'>
+    <div className='bg-[url("/backgrounds/ourTeam/Background_Team.webp")] bg-cover bg-top bg-no-repeat min-h-screen'>
       <Header />
       <div className='w-full'>{children}</div>
       <Footer />
