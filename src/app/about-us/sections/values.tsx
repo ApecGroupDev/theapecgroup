9c8f8246354b815834c838e78947753c593d56dd
Mark Balajadia <markb.freelance@gmail.com>
2025-08-17 22:59:08 +0800

    mission values vision about us page update

--- src/app/about-us/sections/values.tsx
+++ src/app/about-us/sections/values.tsx
@@ -30,7 +30,7 @@ const ValuesSection = () => {
   const { ref, inView } = useInView({ triggerOnce: true });
 
   return (
-    <section className='w-full bg-transparent text-center text-gray-200 border-b-2 border-dashed border-red-700'>
+    <section className='w-full bg-transparent text-center text-gray-200'>
       <div className='flex flex-col mx-auto items-center text-center w-full overflow-hidden max-w-[2560px] pt-12 scrn-500:pt-0 scrn-600:pt-16 scrn-1100:pt-24 scrn-1200:pt-32 scrn-1300:pt-44 scrn-1400:pt-52 h-128 scrn-400:h-132 scrn-500:h-124 scrn-700:h-128 scrn-750:h-124 scrn-800:h-132 scrn-900:h-140 scrn-1000:h-152 scrn-1100:h-168 scrn-1200:h-188 scrn-1300:h-198 scrn-1400:h-216 scrn-1450:h-224 scrn-1500:h-208 scrn-1550:h-216 scrn-1600:h-240 scrn-1650:h-248 scrn-1700:h-256 scrn-1800:h-264 scrn-1850:h-272 scrn-1900:h-272 scrn-2000:h-280 scrn-2100:h-296 scrn-2200:h-308 scrn-2250:h-312 scrn-2300:h-320 scrn-2350:h-326 scrn-2450:h-339 scrn-2550:h-35'>
         <span className='text-6xl text-white scrn-300:text-4xl scrn-300:mt-4 scrn-450:mt-6 scrn-1900:mt-24 scrn-750:text-4xl scrn-800:text-5xl scrn-900:text-5xl scrn-1000:text-5xl scrn-1250:text-7xl scrn-2500:text-9xl italic'>Our</span>
         <motion.span
