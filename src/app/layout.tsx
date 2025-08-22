3b00aec2bd248052416fbfa176dee61d0fcf86a3
Mark Balajadia <markb.freelance@gmail.com>
2025-08-23 04:06:28 +0800

    favicon update, new files and integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,12 @@ export const metadata: Metadata = {
   description: 'The APEC Group is a petroleum products company offering fuels, lubricants, gas station construction, financing, compliance, and equipment services.',
   robots: 'index,follow',
   icons: {
-    icon: '/logos/One-Stop-Shop-Logo.png',
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-48x48.png', sizes: '48x48', type: 'image/png' },
+    ],
+    apple: '/apple-touch-icon.png',
   },
   metadataBase: new URL('https://www.theapecgroup.com'),
   alternates: {
