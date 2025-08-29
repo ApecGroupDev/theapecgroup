acbf29e6c8bf9bcc5009c72062d5977fe0998483
Mark Balajadia <markb.freelance@gmail.com>
2025-08-30 02:29:25 +0800

    layout tsx metadata and head tag content rework, removed separate component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css';
 import ScrollToTopButton from '@/components/scrollToTop';
 import type { Metadata } from 'next';
 import { Analytics } from '@vercel/analytics/next';
-import HeadManager from './HeadManager'; // client wrapper
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Petroleum Products Company | The APEC Group',
@@ -25,10 +25,29 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="bg-gray-300 text-gray-800">
-      <HeadManager />
+      <head>
+        {/* Google Search Console */}
+        <meta
+          name="google-site-verification"
+          content="h5105p5iWJykIV1y3DbAOkWpFei5332BIw9M2XvO72s"
+        />
+      </head>
       <body className="min-h-screen">
         <div className="max-w-[2560px] mx-auto">{children}</div>
         <ScrollToTopButton />
+        {/* ✅ Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-WXLE69Q1SV"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-WXLE69Q1SV');
+        `}
+        </Script>
         {process.env.NODE_ENV === 'production' && <Analytics />}
       </body>
     </html>
