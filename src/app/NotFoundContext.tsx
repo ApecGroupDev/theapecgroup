26e57e51b16ada16ef4bbc5bcd262f081e935676
Mark Balajadia <markb.freelance@gmail.com>
2025-08-30 02:57:24 +0800

    removed notFoundContext separate component

--- src/app/NotFoundContext.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-import { createContext, useContext } from 'react';
-
-export const NotFoundContext = createContext(false);
-
-export const useIsNotFound = () => useContext(NotFoundContext);
