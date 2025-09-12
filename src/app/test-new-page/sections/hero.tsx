645bcecec5151811f231bc511957eaaaef598ef1
Mark Balajadia <markb.freelance@gmail.com>
2025-09-13 01:52:22 +0800

    last push error try fix

--- src/app/test-new-page/sections/hero.tsx
+++ src/app/test-new-page/sections/hero.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect } from 'react';
 // import Image from 'next/image';
 
 const Hero: React.FC = () => {
