0d253fee59a9764d30118504ca57c9f2e3a7b105
Mark Balajadia <markb.freelance@gmail.com>
2025-09-29 01:30:21 +0800

    career page sections margin update

--- src/app/petroleum-companies-jobs/sections/main.tsx
+++ src/app/petroleum-companies-jobs/sections/main.tsx
@@ -91,30 +91,32 @@ const MainFinancing: React.FC = () => {
         </div>
 
         {/* OPEN POSITIONS */}
-        <div className="mt-20">
-          <span className='apec-red font-semibold text-sm scrn-500:text-base scrn-800:text-xl scrn-1500:text-3xl scrn-1700:text-4xl scrn-2000:text-5xl'>
-            NOW HIRING: Petroleum Jobs Near You
-          </span>
-          <p className="italic mt-4 scrn-1000:text-lg scrn-1500:text-xl scrn-1700:text-2xl scrn-1900:text-2xl scrn-2500:text-3xl scrn-800:font-normal scrn-900:font-normal tracking-widest">
-            We’re actively hiring for petroleum companies jobs in operations, compliance, and delivery — with immediate openings like:
-          </p>
-        </div>
-        <div className="mt-8 outline-dashed outline-[#c62931] p-4 rounded-lg text-center">
-          <span className='font-semibold text-sm scrn-500:text-base scrn-800:text-xl scrn-1500:text-3xl scrn-1700:text-4xl scrn-2000:text-5xl'>
-            Service Technician – Atlanta, GA
-          </span>
-          <p className="scrn-1000:text-lg scrn-1500:text-xl scrn-1700:text-2xl scrn-1900:text-2xl scrn-2500:text-3xl scrn-800:font-normal scrn-900:font-normal tracking-widest">
-            Looking for an experienced technician with expertise in POS systems and fuel dispensers. Past certifications and at least
-            two years of experience are required. Must be willing to work long hours and be on-call on weekends.
-          </p>
-          <p className="scrn-1000:text-lg mt-2 scrn-1500:text-xl scrn-1700:text-2xl scrn-1900:text-2xl scrn-2500:text-3xl scrn-800:font-normal scrn-900:font-normal tracking-widest">
-            <span className="apec-red">Location:</span> <span className="font-semibold">Atlanta, GA</span> <br />
-            <span className="apec-red">Type:</span> <span className="font-semibold">Full-Time</span>
-          </p>
+        <div className="py-32">
+          <div>
+            <span className='apec-red font-semibold text-sm scrn-500:text-base scrn-800:text-xl scrn-1500:text-3xl scrn-1700:text-4xl scrn-2000:text-5xl'>
+              NOW HIRING: Petroleum Jobs Near You
+            </span>
+            <p className="italic mt-4 scrn-1000:text-lg scrn-1500:text-xl scrn-1700:text-2xl scrn-1900:text-2xl scrn-2500:text-3xl scrn-800:font-normal scrn-900:font-normal tracking-widest">
+              We’re actively hiring for petroleum companies jobs in operations, compliance, and delivery — with immediate openings like:
+            </p>
+          </div>
+          <div className="mt-8 outline-dashed outline-[#c62931] p-4 rounded-lg text-center">
+            <span className='font-semibold text-sm scrn-500:text-base scrn-800:text-xl scrn-1500:text-3xl scrn-1700:text-4xl scrn-2000:text-5xl'>
+              Service Technician – Atlanta, GA
+            </span>
+            <p className="scrn-1000:text-lg scrn-1500:text-xl scrn-1700:text-2xl scrn-1900:text-2xl scrn-2500:text-3xl scrn-800:font-normal scrn-900:font-normal tracking-widest">
+              Looking for an experienced technician with expertise in POS systems and fuel dispensers. Past certifications and at least
+              two years of experience are required. Must be willing to work long hours and be on-call on weekends.
+            </p>
+            <p className="scrn-1000:text-lg mt-2 scrn-1500:text-xl scrn-1700:text-2xl scrn-1900:text-2xl scrn-2500:text-3xl scrn-800:font-normal scrn-900:font-normal tracking-widest">
+              <span className="apec-red">Location:</span> <span className="font-semibold">Atlanta, GA</span> <br />
+              <span className="apec-red">Type:</span> <span className="font-semibold">Full-Time</span>
+            </p>
+          </div>
         </div>
 
         {/* STEPS TO APPLY */}
-        <section className="w-full mt-12 space-y-8">
+        <section className="w-full mt-12 space-y-8 pb-24">
           <div>
             <span className='apec-red font-semibold text-sm scrn-500:text-base scrn-800:text-xl scrn-1500:text-3xl scrn-1700:text-4xl scrn-2000:text-5xl'>
               How to Apply
@@ -138,6 +140,7 @@ const MainFinancing: React.FC = () => {
             ))}
           </div>
         </section>
+
         <div className='relative rounded-3xl shadow-2xl mt-6 scrn-500:mt-8'>
           <div className='absolute inset-0 bg-white/30 backdrop-blur-md rounded-3xl z-0'></div>
           <div className='relative text-center z-10 p-4 scrn-800:p-8 scrn-900:p-10 scrn-1000:p-12 scrn-1250:p-16 scrn-1500:p-24'>
