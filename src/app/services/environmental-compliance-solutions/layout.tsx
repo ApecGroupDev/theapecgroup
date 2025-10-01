dd0a5747ba9b599d63cda65aebdd26086d30e01a
Mark Balajadia <markb.freelance@gmail.com>
2025-10-01 23:07:15 +0800

    services main and subpages layout update, bg to white

--- src/app/services/environmental-compliance-solutions/layout.tsx
+++ src/app/services/environmental-compliance-solutions/layout.tsx
@@ -1,5 +1,5 @@
 export default function ServicesComplianceLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='bg-gray-200 w-full'>{children}</div>
+    <div className='w-full'>{children}</div>
   );
 }
