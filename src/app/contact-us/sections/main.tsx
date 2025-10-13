9c64d8f6f805d4467ed466094eff982d3b39b382
Mark Balajadia <markb.freelance@gmail.com>
2025-10-13 13:44:36 +0800

    contact us page fonts update

--- src/app/contact-us/sections/main.tsx
+++ src/app/contact-us/sections/main.tsx
@@ -18,11 +18,9 @@ const MainImaging: React.FC = () => {
     <div className='bg-transparent flex flex-col items-center scrn-600:justify-center h-auto max-w-[1440px] mx-auto px-4 scrn-600:px-6 scrn-1000:px-8'>
       <div className='mt-12 grid scrn-600:grid-cols-2 scrn-600:gap-x-6 scrn-750:gap-x-10 scrn-1200:gap-x-24'>
         <div className='h-auto space-y-3 scrn-500:space-y-4 scrn-1200:space-y-12 pt-4 scrn-400:pt-8 scrn-600:pt-16 scrn-1000:pt-20 scrn-1600:pt-32'>
-          <div>
-            <span className='text-[#c62931] font-semibold text-sm scrn-500:text-base scrn-800:text-xl scrn-1500:text-3xl scrn-1700:text-4xl scrn-2000:text-5xl'>
-              What We Can Help You With:
-            </span>
-          </div>
+          <h2 className="text-4xl font-extrabold text-gray-900 mb-12">
+            What We Can <span className="text-[#c62931]">Help You With:</span>
+          </h2>
           <div className="space-y-3 scrn-1000:space-y-6 tracking-widest">
             {badges.map((text, i) => (
               <CheckmarkBadgeNew key={i} text={text} />
