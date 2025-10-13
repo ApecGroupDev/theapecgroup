b0434327726c66a7acbe08917e3ec7d3f87d45a9
Mark Balajadia <markb.freelance@gmail.com>
2025-10-13 17:13:37 +0800

    contact us page spacing update

--- src/app/contact-us/sections/main.tsx
+++ src/app/contact-us/sections/main.tsx
@@ -16,12 +16,12 @@ const badges = [
 const MainImaging: React.FC = () => {
   return (
     <div className='bg-transparent flex flex-col items-center scrn-600:justify-center h-auto max-w-[1440px] mx-auto px-4 scrn-600:px-6 scrn-1000:px-8'>
-      <div className='mt-12 grid scrn-600:grid-cols-2 scrn-600:gap-x-6 scrn-750:gap-x-10 scrn-1200:gap-x-24'>
-        <div className='h-auto space-y-3 scrn-500:space-y-4 scrn-1200:space-y-12 pt-4 scrn-400:pt-8 scrn-600:pt-16 scrn-1000:pt-20 scrn-1600:pt-32'>
-          <h2 className="text-4xl font-extrabold text-gray-900 mb-8">
+      <div className='mt-12 grid scrn-800:grid-cols-2 scrn-800:gap-x-6 scrn-750:gap-x-10 scrn-1200:gap-x-24'>
+        <div className='h-auto scrn-800:mt-16 scrn-1000:mt-20 scrn-1250:mt-24 scrn-1500:mt-32'>
+          <h2 className="text-4xl font-extrabold text-gray-900">
             What We Can <span className="text-[#c62931]">Help You With:</span>
           </h2>
-          <div className="space-y-3 scrn-1000:space-y-6 tracking-widest">
+          <div className="space-y-3 scrn-1000:space-y-6 tracking-widest mt-4">
             {badges.map((text, i) => (
               <CheckmarkBadgeNew key={i} text={text} />
             ))}
