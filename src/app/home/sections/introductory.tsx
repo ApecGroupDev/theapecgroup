80359fc7af57d44b4b01e653583c9acaf7afb295
Mark Balajadia <markb.freelance@gmail.com>
2025-10-15 02:37:32 +0800

    homepage introductory update

--- src/app/home/sections/introductory.tsx
+++ src/app/home/sections/introductory.tsx
@@ -25,39 +25,37 @@ const Introductory: React.FC = () => {
     <div className='bg-transparent flex flex-col items-center scrn-600:justify-center h-auto max-w-[1440px] mx-auto px-4 scrn-600:px-6 scrn-1000:px-8'>
       <div className="flex flex-col items-center scrn-600:justify-center gap-y-4 mb-12">
         <div className="mt-12 space-y-3">
-          <p className="tracking-widest text-lg">
+          <p className="tracking-wider text-lg">
             When you’re managing a fueling operation, the stakes are high. A mistake could mean costly fines, lost customers, or even shutdowns.
             At <span className="apec-red font-semibold">The APEC Group(Atlanta Petroleum Equipment Company)</span>, we don’t just supply petroleum
             products —we are a petroleum products company that helps protect what you’ve built. Since 1989, we’ve partnered with fuel retailers,
             fleet operators, and distributors across Georgia to deliver safe, compliant, and reliable fueling solutions.
           </p>
-          <p className="tracking-widest apec-red font-semibold text-lg">
+          <p className="tracking-wider apec-red italic text-lg">
             Looking for a partner who handles the complexity so you can focus on running your business? That’s what we do.
           </p>
         </div>
 
         <div className='h-auto space-y-3 scrn-500:space-y-4 scrn-1200:space-y-12 pt-12 w-full mb-12'>
-          <div className="grid scrn-600:grid-cols-2 scrn-600:gap-x-6 scrn-750:gap-x-10 scrn-1200:gap-x-24">
+          <div className="grid scrn-800:grid-cols-2 scrn-800:gap-x-6 scrn-750:gap-x-10 scrn-1200:gap-x-24">
             <div className='scrn-300:text-xs scrn-400:text-sm scrn-800:text-base scrn-1000:text-lg scrn-1500:text-xl scrn-1700:text-2xl scrn-2500:text-3xl space-y-2'>
-              <h2 className="mb-8">
-                <span className='font-semibold uppercase scrn-800:text-xl scrn-1500:text-3xl'>
-                  Core Value Points
-                </span>
+              <h2 className="text-4xl font-extrabold text-gray-900">
+                Core Value <span className="text-[#c62931]">Points</span>
               </h2>
-              {checklistItems.map((item, index) => (
-                <div key={index} className="flex items-center gap-2 tracking-widest">
-                  <Check className="w-6 h-6 flex-shrink-0 text-[#c62931]" />
-                  <span className="scrn-1000:text-lg scrn-1500:text-xl">{item}</span>
-                </div>
-              ))}
-            </div>
-            <div>
-              <div className="mt-12 scrn-600:mt-0 mb-8">
-                <span className='font-semibold text-sm uppercase scrn-800:text-xl scrn-1500:text-3xl'>
-                  Our Petroleum Products & Services:
-                </span>
+              <div className="flex flex-wrap gap-x-20 gap-y-4 mt-4">
+                {checklistItems.map((item, index) => (
+                  <div key={index} className="flex items-center gap-2 tracking-wider">
+                    <Check className="w-6 h-6 flex-shrink-0 text-[#c62931]" />
+                    <span className="scrn-1000:text-lg scrn-1500:text-xl">{item}</span>
+                  </div>
+                ))}
               </div>
-              <div className="space-y-3 scrn-1000:space-y-6 tracking-widest">
+            </div>
+            <div className="mt-12 scrn-800:mt-0">
+              <h2 className="text-4xl font-extrabold text-gray-900">
+                Our Petroleum <span className="text-[#c62931]">Products & Services:</span>
+              </h2>
+              <div className="space-y-3 scrn-1000:space-y-6 tracking-wider mt-4">
                 {badges.map((text, i) => (
                   <CheckmarkBadgeNew key={i} text={text} />
                 ))}
