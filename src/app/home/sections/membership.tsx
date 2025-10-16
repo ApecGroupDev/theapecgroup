5f5a0d83616469d7288767669afc13dfc1cf6578
Mark Balajadia <markb.freelance@gmail.com>
2025-10-17 03:46:50 +0800

    membership logos link to website update

--- src/app/home/sections/membership.tsx
+++ src/app/home/sections/membership.tsx
@@ -1,17 +1,46 @@
 'use client';
 
 import Image from 'next/image';
+import Link from 'next/link';
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 
 const associations = [
-  { src: "/associations/ARA.webp", alt: "ARA - Atlanta Retailers Association" },
-  { src: "/associations/GACS.webp", alt: "GACS - Georgia Association of Convenience Stores" },
-  { src: "/associations/GHRA.webp", alt: "GHRA - Greater Houston Retailers Cooperative Association, Inc." },
-  { src: "/associations/GOA.webp", alt: "GOA - Georgia Oilman’s Association" },
-  { src: "/associations/GTEC.webp", alt: "GTEC - Georgia Tank Environmental Contractors Association" },
-  { src: "/associations/PEI.webp", alt: "PEI - Petroleum Equipment Institute" },
-  { src: "/associations/TFFA.webp", alt: "TFFA - Texas Food & Fuel Association" },
+  {
+    src: "/associations/ARA.webp",
+    alt: "ARA - Atlanta Retailers Association",
+    link: "https://www.araonline.us/",
+  },
+  {
+    src: "/associations/GACS.webp",
+    alt: "GACS - Georgia Association of Convenience Stores",
+    link: "https://www.gacs.com/",
+  },
+  {
+    src: "/associations/GHRA.webp",
+    alt: "GHRA - Greater Houston Retailers Cooperative Association, Inc.",
+    link: "https://www.ghraonline.com/",
+  },
+  {
+    src: "/associations/GOA.webp",
+    alt: "GOA - Georgia Oilman’s Association",
+    link: "https://www.georgiaoilmensassoc.com/",
+  },
+  {
+    src: "/associations/GTEC.webp",
+    alt: "GTEC - Georgia Tank Environmental Contractors Association",
+    link: "https://gteca.com/",
+  },
+  {
+    src: "/associations/PEI.webp",
+    alt: "PEI - Petroleum Equipment Institute",
+    link: "https://www.pei.org/",
+  },
+  {
+    src: "/associations/TFFA.webp",
+    alt: "TFFA - Texas Food & Fuel Association",
+    link: "https://www.tffa.com/",
+  },
 ];
 
 const MembershipGrid = () => {
@@ -45,18 +74,24 @@ const MembershipGrid = () => {
           initial={{ opacity: 0, y: 30 }}
           animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 30 }}
           transition={{ duration: 0.8, ease: "easeOut" }}
-          className="grid grid-cols-2 scrn-600:grid-cols-3 scrn-1000:grid-cols-4 gap-y-12 gap-x-10 items-center justify-items-center justify-center [grid-auto-flow:row]">
+          className="grid grid-cols-2 scrn-600:grid-cols-3 scrn-1000:grid-cols-4 gap-y-12 gap-x-10 items-center justify-items-center justify-center [grid-auto-flow:row]"
+        >
           {associations.map((assoc, index) => (
-            <div
+            <Link
               key={index}
-              className="flex items-center justify-center transition-all duration-300 hover:scale-125">
+              href={assoc.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center justify-center transition-all duration-300 hover:scale-125"
+            >
               <Image
                 src={assoc.src}
                 alt={assoc.alt}
                 width={250}
                 height={250}
-                className="object-contain w-[160px] scrn-600:w-[180px] scrn-750:w-[200px] scrn-1000:w-[230px] scrn-1500:w-[260px]"/>
-            </div>
+                className="object-contain w-[160px] scrn-600:w-[180px] scrn-750:w-[200px] scrn-1000:w-[230px] scrn-1500:w-[260px]"
+              />
+            </Link>
           ))}
         </motion.div>
 
