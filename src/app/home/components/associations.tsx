d8bae62593208a0761dc28769c07709261d458b0
Mark Balajadia <markb.freelance@gmail.com>
2026-02-27 08:52:20 +0800

    new component separated associations for homepage

---
+++ src/app/home/components/associations.tsx
@@ -0,0 +1,45 @@
+// data/associations.ts
+
+export interface Association {
+  src: string;
+  alt: string;
+  link: string;
+}
+
+export const associations: Association[] = [
+  {
+    src: "/associations/ARA.webp",
+    alt: "Atlanta Retailers Association (ARA) Logo",
+    link: "https://www.araonline.us/",
+  },
+  {
+    src: "/associations/GACS.webp",
+    alt: "Georgia Association of Convenience Stores (GACS) Logo",
+    link: "https://www.gacs.com/",
+  },
+  {
+    src: "/associations/GHRA.webp",
+    alt: "Greater Houston Retailers Cooperative Association (GHRA) Logo",
+    link: "https://www.ghraonline.com/",
+  },
+  {
+    src: "/associations/GOA.webp",
+    alt: "Georgia Oilmen's Association (GOA) Logo",
+    link: "https://www.georgiaoilmensassoc.com/",
+  },
+  {
+    src: "/associations/GTEC.webp",
+    alt: "Georgia Tank & Environmental Contractors Association (GTECA) Logo",
+    link: "https://gteca.com/",
+  },
+  {
+    src: "/associations/PEI.webp",
+    alt: "Petroleum Equipment Institute (PEI) Logo",
+    link: "https://www.pei.org/",
+  },
+  {
+    src: "/associations/TFFA.webp",
+    alt: "Texas Food & Fuel Association (TFFA) Logo",
+    link: "https://www.tffa.com/",
+  },
+];
