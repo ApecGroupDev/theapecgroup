37e6cb102c14b02f8a470053641d21789a214bef
Mark Balajadia <markb.freelance@gmail.com>
2026-08-08 03:49:07 +0800

    chore: remove Sentry widenClientFileUpload to speed up vercel build and hide warnings

--- next.config.ts
+++ next.config.ts
@@ -44,13 +44,10 @@ export default withSentryConfig(nextConfig, {
   project: "theapecgroup",
 
   // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
+  silent: true,
 
   // For all available options, see:
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
 
   // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
   // This can increase your server load as well as your hosting bill.
